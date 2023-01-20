6eedceadd7de9a46b9bdbc7f98d6a2b567c1b01b
BalaSPuli <bspuli@enquizit.com>
2023-01-20 11:55:30 -0500

    Initial commit

---
+++ report-service/src/main/java/gov/cdc/dataingestion/report/config/MongoDBConfig.java
@@ -0,0 +1,30 @@
+package gov.cdc.dataingestion.report.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+/***
+ *
+ */
+@Configuration
+public class MongoDBConfig {
+
+    /**
+     * mongodb config
+     *
+     * @param databaseFactory factory
+     * @param converter converter
+     * @return
+     */
+    @Bean
+    public MongoTemplate mongoTemplate(
+            final MongoDatabaseFactory databaseFactory,
+                                       final MappingMongoConverter converter) {
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return new MongoTemplate(databaseFactory, converter);
+    }
+
+}
