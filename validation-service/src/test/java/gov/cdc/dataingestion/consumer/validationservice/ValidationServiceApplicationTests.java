531d4102b6f2ba7c6ccee8b335a0662677246c0a
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-03-21 09:19:35 -0700

    added package

---
+++ validation-service/src/test/java/gov/cdc/dataingestion/consumer/validationservice/ValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cdc.dataingestion.consumer.validationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
