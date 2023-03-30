3e418ebc16ee184a0d244b0b79b5ea79140aa7e3
BalaSPuli <bspuli@enquizit.com>
2023-03-30 13:45:09 -0400

    CNDIT-225: HL7 end point

---
+++ report-service/src/main/java/gov/cdc/dataingestion/rawmessage/service/RawELRService.java
@@ -0,0 +1,60 @@
+package gov.cdc.dataingestion.rawmessage.service;
+
+import gov.cdc.dataingestion.rawmessage.dto.RawERLDto;
+import gov.cdc.dataingestion.validation.integration.service.KafkaProducerService;
+import gov.cdc.dataingestion.validation.model.RawERLModel;
+import gov.cdc.dataingestion.validation.repository.RawELRRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RawELRService {
+
+    private static final String CREATED_BY = "admin";
+    @Value("${kafka.raw.producer.topic}")
+    String topicName;
+    private final RawELRRepository rawELRRepository;
+    private final KafkaProducerService kafkaProducerService;
+
+
+    public String submission(RawERLDto rawERLDto) {
+        RawERLModel created = rawELRRepository.save(convert(rawERLDto));
+        kafkaProducerService.sendMessageFromController(created.getId(), topicName, rawERLDto.getType());
+        return created.getId();
+    }
+
+    public RawERLDto getById(String id) {
+        RawERLModel rawERLModel = rawELRRepository.getById(id);
+        return convert(rawERLModel);
+    }
+
+    private RawERLModel convert(RawERLDto rawERLDto) {
+
+        RawERLModel rawERLModel = new RawERLModel();
+        rawERLModel.setId(UUID.randomUUID().toString());
+        rawERLModel.setType(rawERLDto.getType());
+        rawERLModel.setPayload(rawERLDto.getPayload());
+        rawERLModel.setCreatedOn(Timestamp.from(ZonedDateTime.now().toInstant()));
+        rawERLModel.setUpdatedOn(Timestamp.from(ZonedDateTime.now().toInstant()));
+        rawERLModel.setCreatedBy(CREATED_BY);
+        rawERLModel.setUpdatedBy(CREATED_BY);
+        return rawERLModel;
+    }
+
+    private RawERLDto convert(RawERLModel rawERLModel) {
+
+        RawERLDto rawERLDto = new RawERLDto();
+        rawERLDto.setId(rawERLModel.getId());
+        rawERLDto.setType(rawERLModel.getType());
+        rawERLDto.setPayload(rawERLModel.getPayload());
+        return rawERLDto;
+    }
+}
