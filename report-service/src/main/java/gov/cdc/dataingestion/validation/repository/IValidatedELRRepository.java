2df26d313e0b90884af582712db19d20874a1094
Ragul Shanmugam <123001929+rshanmugam-enquizit@users.noreply.github.com>
2023-04-12 22:39:14 -0700

    [CNDIT-232] Deduplication algorithm to check HL7 duplicate (#27)
    * HL7 Deduplication basic algorithm using SHA-256 encryption
    
    * Unit tests added
    
    * Reverting ReportControllerTest changes
    
    * Duplicate kafka topic name and test method name fix plus more refactoring

--- report-service/src/main/java/gov/cdc/dataingestion/validation/repository/IValidatedELRRepository.java
+++ report-service/src/main/java/gov/cdc/dataingestion/validation/repository/IValidatedELRRepository.java
@@ -4,6 +4,10 @@ import gov.cdc.dataingestion.validation.repository.model.ValidatedELRModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IValidatedELRRepository extends JpaRepository<ValidatedELRModel, String> {
+    Optional<ValidatedELRModel> findByHashedHL7String(String hashedString);
+
 }
---
+++ report-service/src/test/java/gov/cdc/dataingestion/validation/integration/validator/HL7DuplicateValidatorTest.java
@@ -0,0 +1,103 @@
+package gov.cdc.dataingestion.validation.integration.validator;
+
+import gov.cdc.dataingestion.exception.DuplicateHL7FileFoundException;
+import gov.cdc.dataingestion.kafka.integration.service.KafkaProducerService;
+import gov.cdc.dataingestion.validation.repository.IValidatedELRRepository;
+import gov.cdc.dataingestion.validation.repository.model.ValidatedELRModel;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class HL7DuplicateValidatorTest {
+
+    HL7DuplicateValidator hl7DuplicateValidator;
+    IValidatedELRRepository iValidatedELRRepositoryMock;
+    KafkaProducerService kafkaProducerServiceMock;
+
+    @BeforeEach
+    void setUp() {
+        iValidatedELRRepositoryMock = mock(IValidatedELRRepository.class);
+        kafkaProducerServiceMock = mock(KafkaProducerService.class);
+        hl7DuplicateValidator = new HL7DuplicateValidator(iValidatedELRRepositoryMock, kafkaProducerServiceMock);
+    }
+
+    @AfterEach
+    void tearDown() {
+        Mockito.reset(iValidatedELRRepositoryMock);
+    }
+
+    @Test
+    void testValidateHL7DocumentSuccess() throws DuplicateHL7FileFoundException {
+        ValidatedELRModel validatedELRModel = getValidatedELRModel();
+
+        Mockito.when(iValidatedELRRepositoryMock.findByHashedHL7String(any()))
+                .thenReturn(Optional.empty());
+        hl7DuplicateValidator.ValidateHL7Document(validatedELRModel);
+
+        assertNotNull(validatedELRModel.getHashedHL7String());
+        verify(iValidatedELRRepositoryMock).findByHashedHL7String(any());
+    }
+
+    @Test
+    void testValidateHL7DocumentThrowsException() {
+        String hashedString = "843588fcbfbdca29f9807f81455bbd3ae6935dae6152bcac6851e9568c885c66";
+        ValidatedELRModel validatedELRModel = getValidatedELRModel();
+
+        Mockito.when(iValidatedELRRepositoryMock.findByHashedHL7String(hashedString))
+                .thenReturn(Optional.of(validatedELRModel));
+        validatedELRModel.setHashedHL7String(hashedString);
+
+        doNothing().when(kafkaProducerServiceMock).sendMessageAfterCheckingDuplicateHL7(any(ValidatedELRModel.class), anyString());
+
+        assertThrows(DuplicateHL7FileFoundException.class, () -> hl7DuplicateValidator.ValidateHL7Document(validatedELRModel));
+        verify(iValidatedELRRepositoryMock).findByHashedHL7String(hashedString);
+    }
+
+    @Test
+    void testCheckDuplicateHL7Exists() {
+        String hashedString = "843588fcbfbdca29f9807f81455bbd3ae6935dae6152bcac6851e9568c885c66";
+        ValidatedELRModel validatedELRModel = getValidatedELRModel();
+        validatedELRModel.setHashedHL7String(hashedString);
+
+        Mockito.when(iValidatedELRRepositoryMock.findByHashedHL7String(hashedString))
+                .thenReturn(Optional.of(validatedELRModel));
+        boolean result = hl7DuplicateValidator.checkForDuplicateHL7HashString(hashedString);
+
+        assertTrue(result);
+        verify(iValidatedELRRepositoryMock).findByHashedHL7String(hashedString);
+    }
+
+    @Test
+    void testCheckDuplicateHL7NotExists() {
+        Mockito.when(iValidatedELRRepositoryMock.findByHashedHL7String(any()))
+                .thenReturn(Optional.empty());
+        boolean result = hl7DuplicateValidator.checkForDuplicateHL7HashString(any());
+
+        assertFalse(result);
+        verify(iValidatedELRRepositoryMock).findByHashedHL7String(any());
+    }
+
+    @NotNull
+    private static ValidatedELRModel getValidatedELRModel() {
+        String data = "MSH|^~\\&|ULTRA|TML|OLIS|OLIS|200905011130||ORU^R01|20169838-v25|T|2.5\r"
+                + "PID|||7005728^^^TML^MR||TEST^RACHEL^DIAMOND||19310313|F|||200 ANYWHERE ST^^TORONTO^ON^M6G 2T9||(416)888-8888||||||1014071185^KR\r"
+                + "PV1|1||OLIS||||OLIST^BLAKE^DONALD^THOR^^^^^921379^^^^OLIST\r"
+                + "ORC|RE||T09-100442-RET-0^^OLIS_Site_ID^ISO|||||||||OLIST^BLAKE^DONALD^THOR^^^^L^921379\r"
+                + "OBR|0||T09-100442-RET-0^^OLIS_Site_ID^ISO|RET^RETICULOCYTE COUNT^HL79901 literal|||200905011106|||||||200905011106||OLIST^BLAKE^DONALD^THOR^^^^L^921379||7870279|7870279|T09-100442|MOHLTC|200905011130||B7|F||1^^^200905011106^^R\r"
+                + "OBX|1|ST|||TestValue";
+
+        ValidatedELRModel validatedELRModel = new ValidatedELRModel();
+        validatedELRModel.setRawId("test_uuid");
+        validatedELRModel.setRawMessage(data);
+        validatedELRModel.setMessageType("HL7");
+        return validatedELRModel;
+    }
+}
