b14946d193e088ad1fbfd8997d9bcf370951efd1
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-04-25 16:02:13 -0700

    added DSC

--- hl7-helper/hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/model/hl7/messageType/OruR1.java
+++ hl7-helper/hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/model/hl7/messageType/OruR1.java
@@ -2,6 +2,7 @@ package gov.cdc.dataingestion.hl7.helper.model.hl7.messageType;
 
 import ca.uhn.hl7v2.HL7Exception;
 import gov.cdc.dataingestion.hl7.helper.model.hl7.messageGroup.PatientResult;
+import gov.cdc.dataingestion.hl7.helper.model.hl7.messageSegment.ContinuationPointer;
 import gov.cdc.dataingestion.hl7.helper.model.hl7.messageSegment.MessageHeader;
 import gov.cdc.dataingestion.hl7.helper.model.hl7.messageSegment.SoftwareSegment;
 import lombok.Getter;
@@ -16,6 +17,7 @@ public class OruR1 {
     MessageHeader messageHeader;
     List<SoftwareSegment> softwareSegment;
     List<PatientResult> patientResult;
+    ContinuationPointer continuationPointer;
 
     public OruR1(ca.uhn.hl7v2.model.v251.message.ORU_R01 oruR01) throws HL7Exception {
 
@@ -28,7 +30,7 @@ public class OruR1 {
         for(var item : oruR01.getPATIENT_RESULTAll()) {
             this.patientResult.add(new PatientResult(item));
         }
-
+        this.continuationPointer = new ContinuationPointer(oruR01.getDSC());
 
     }
 }
