163ac62629fd62cd906f9542d0c6218cd73983a4
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-04-25 14:16:27 -0700

    added clinical trial identification

---
+++ hl7-helper/hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/model/hl7/group/order/ClinicalTrialIdentification.java
@@ -0,0 +1,20 @@
+package gov.cdc.dataingestion.hl7.helper.model.hl7.group.order;
+import gov.cdc.dataingestion.hl7.helper.model.hl7.messageDataType.Ce;
+import gov.cdc.dataingestion.hl7.helper.model.hl7.messageDataType.Ei;
+import lombok.Getter;
+import lombok.Setter;
+import static gov.cdc.dataingestion.hl7.helper.helper.modelListHelper.*;
+import java.util.List;
+
+@Getter
+@Setter
+public class ClinicalTrialIdentification {
+    Ei sponsorStudyId;
+    Ce studyPhaseIdentifier;
+    Ce studyScheduledTimePoint;
+    public ClinicalTrialIdentification(ca.uhn.hl7v2.model.v251.segment.CTI cti) {
+        this.sponsorStudyId = new Ei(cti.getSponsorStudyID());
+        this.studyPhaseIdentifier = new Ce(cti.getStudyPhaseIdentifier());
+        this.studyScheduledTimePoint = new Ce(cti.getCti3_StudyScheduledTimePoint());
+    }
+}
