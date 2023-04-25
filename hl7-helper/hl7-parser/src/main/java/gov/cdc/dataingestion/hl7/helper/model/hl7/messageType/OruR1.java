403b7d77c35ca0494d910f164f8adc07a4643bdb
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-04-25 16:00:23 -0700

    added software segment

--- hl7-helper/hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/model/hl7/messageType/OruR1.java
+++ hl7-helper/hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/model/hl7/messageType/OruR1.java
@@ -3,6 +3,7 @@ package gov.cdc.dataingestion.hl7.helper.model.hl7.messageType;
 import ca.uhn.hl7v2.HL7Exception;
 import gov.cdc.dataingestion.hl7.helper.model.hl7.messageGroup.PatientResult;
 import gov.cdc.dataingestion.hl7.helper.model.hl7.messageSegment.MessageHeader;
+import gov.cdc.dataingestion.hl7.helper.model.hl7.messageSegment.SoftwareSegment;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,15 +14,21 @@ import java.util.List;
 @Setter
 public class OruR1 {
     MessageHeader messageHeader;
+    List<SoftwareSegment> softwareSegment;
     List<PatientResult> patientResult;
 
     public OruR1(ca.uhn.hl7v2.model.v251.message.ORU_R01 oruR01) throws HL7Exception {
 
         this.messageHeader = new MessageHeader(oruR01.getMSH());
+        this.softwareSegment = new ArrayList<>();
+        for(var item : oruR01.getSFTAll()) {
+            this.softwareSegment.add(new SoftwareSegment(item));
+        }
         this.patientResult = new ArrayList<>();
         for(var item : oruR01.getPATIENT_RESULTAll()) {
             this.patientResult.add(new PatientResult(item));
         }
+
 
     }
 }
