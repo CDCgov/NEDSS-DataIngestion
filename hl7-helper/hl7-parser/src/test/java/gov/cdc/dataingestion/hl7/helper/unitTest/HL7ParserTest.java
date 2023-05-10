406ad2c5db791f70799714d7c1368eeaddffdd20
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-05-10 11:06:16 -0700

    cleanup

--- hl7-helper/hl7-parser/src/test/java/gov/cdc/dataingestion/hl7/helper/unitTest/HL7ParserTest.java
+++ hl7-helper/hl7-parser/src/test/java/gov/cdc/dataingestion/hl7/helper/unitTest/HL7ParserTest.java
@@ -53,7 +53,7 @@ public class HL7ParserTest {
 
     @Test
     public void hl7StringParser_ReturnValidMessage() throws  DiHL7Exception {
-        var result = target.hl7StringParser(OruR1RhapsodyConverted251Message_ComplexMicro);
+        var result = target.hl7StringParser(messageByRhapsody);
         Gson gson = new Gson();
         var str = gson.toJson(result);
         Assertions.assertEquals("R01", result.getEventTrigger());
@@ -62,7 +62,7 @@ public class HL7ParserTest {
 
     @Test
     public void hl7StringConvert231To251_ReturnValidMessage() throws  DiHL7Exception {
-        var result = target.convert231To251(OruR1231Message1);
+        var result = target.convert231To251(messageOriginal);
         Gson gson = new Gson();
         var str = gson.toJson(result);
         Assertions.assertEquals("R01", result.getEventTrigger());
