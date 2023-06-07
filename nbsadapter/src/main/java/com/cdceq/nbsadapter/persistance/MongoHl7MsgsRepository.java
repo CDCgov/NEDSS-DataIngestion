d8b3deb151dc8634b33f99703ae982572d68c4e3
Ramesh Addanki <118755149+raddanki-eq@users.noreply.github.com>
2023-06-07 16:23:55 -0400

    CNDIR-419: Moved to Sandbox directory (#46)
    * CNDIR-419: Moved to Sandbox directory
    
    CNDIT-419 : Moved to Sandbox directory
    
    * More code cleanup's
    
    More code cleanup's
    
    ---------
    
    Co-authored-by: Ramesh Addanki <84991783+raddanki64@users.noreply.github.com>

--- nbsadapter/src/main/java/com/cdceq/nbsadapter/persistance/MongoHl7MsgsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cdceq.nbsadapter.persistance;
-
-import  org.springframework.data.mongodb.repository.MongoRepository;
-import  com.cdceq.nbsadapter.persistance.model.EntityHl7Message;
-
-public interface MongoHl7MsgsRepository extends MongoRepository<EntityHl7Message, String> {
-    // 
-}
