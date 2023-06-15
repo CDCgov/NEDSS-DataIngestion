8dd47a19fd63a3b2f196dbad586de5a67862a346
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-06-15 01:15:29 -0700

    added custom response for 401

---
+++ report-service/src/main/java/gov/cdc/dataingestion/share/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,16 @@
+package gov.cdc.dataingestion.share;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write("{ \"error\": \"Unauthorized\", \"message\": \"Unauthorized access\" }");
+    }
+}
