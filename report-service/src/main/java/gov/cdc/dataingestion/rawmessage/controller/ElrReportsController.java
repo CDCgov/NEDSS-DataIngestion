0944bc6c74d385c94f6cccd524961250e160a6b1
Bala Puli <118308699+bspuli@users.noreply.github.com>
2023-06-27 13:31:30 -0400

    CNDIT-412: Add openAPI and Swagger documentation for existing APIs. (#54)
    Co-authored-by: Stefan Deusch <uba2@cdc.gov>

--- report-service/src/main/java/gov/cdc/dataingestion/rawmessage/controller/ElrReportsController.java
+++ report-service/src/main/java/gov/cdc/dataingestion/rawmessage/controller/ElrReportsController.java
@@ -2,6 +2,8 @@ package gov.cdc.dataingestion.rawmessage.controller;
 
 import gov.cdc.dataingestion.rawmessage.dto.RawERLDto;
 import gov.cdc.dataingestion.rawmessage.service.RawELRService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import gov.cdc.dataingestion.share.model.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "ELR Reports", description = "ELR reports API")
+
 @RestController
 @RequestMapping("/api/reports")
 @Slf4j
@@ -19,6 +23,10 @@ public class ElrReportsController {
 
     private final RawELRService rawELRService;
 
+    @Operation(
+            summary = "Submit a plain text HL7 message",
+            description = "Submit a plain text HL7 message with msgType header",
+            tags = { "dataingestion", "elr" })
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<?> save(@RequestBody final String payload, @RequestHeader("msgType") String type) {
             RawERLDto rawERLDto = new RawERLDto();
@@ -27,6 +35,10 @@ public class ElrReportsController {
             return ResponseEntity.ok(rawELRService.submission(rawERLDto));
     }
 
+    @Operation(
+            summary = "Get a report information by id",
+            description = "Get a HL7 report by the given id",
+            tags = { "dataingestion", "elr" })
     @GetMapping(path = "/{id}")
     public ResponseEntity<RawERLDto> getById(@PathVariable String id) {
         return ResponseEntity.ok(rawELRService.getById(id));
