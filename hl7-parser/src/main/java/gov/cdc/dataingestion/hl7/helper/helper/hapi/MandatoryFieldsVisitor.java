f5f6c5c371f60200141f29105800913a23cc363f
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-08-29 13:19:50 -0700

    added validation in HL7 package

---
+++ hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/helper/hapi/MandatoryFieldsVisitor.java
@@ -0,0 +1,28 @@
+package gov.cdc.dataingestion.hl7.helper.helper.hapi;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.Location;
+import ca.uhn.hl7v2.model.Group;
+import ca.uhn.hl7v2.validation.builder.support.ValidatingMessageVisitor;
+import static gov.cdc.dataingestion.hl7.helper.constant.hl7.EventTrigger.ORU_01;
+import static gov.cdc.dataingestion.hl7.helper.constant.hl7.MessageType.ORU;
+import static gov.cdc.dataingestion.hl7.helper.helper.validator.OruR01Validator.patientResultValidator;
+
+public class MandatoryFieldsVisitor extends ValidatingMessageVisitor {
+    private String targetType = "";
+    public MandatoryFieldsVisitor(String targetType) {
+        this.targetType = targetType;
+    }
+    @Override
+    public boolean start(Group group, Location location) throws HL7Exception {
+        if (this.targetType.equalsIgnoreCase(ORU + "_" + ORU_01)) {
+            patientResultValidator(group);
+        }
+        return false;
+    }
+
+
+
+
+
+}
---
+++ hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/helper/validator/OruR01Validator.java
@@ -0,0 +1,63 @@
+package gov.cdc.dataingestion.hl7.helper.helper.validator;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Group;
+import ca.uhn.hl7v2.model.Segment;
+
+public class OruR01Validator {
+    public static void patientResultValidator(Group group) throws HL7Exception {
+        var groupName = group.getName();
+        if(groupName.equalsIgnoreCase("PATIENT_RESULT")) {
+            /// PATIENT
+            patientValidator(group);
+            // ORDER OBSERVATION
+            orderObservationValidator(group);
+
+        }
+    }
+
+    private static void patientValidator(Group group) throws HL7Exception {
+        var patientCounter = group.getAll("PATIENT").length;
+        if (patientCounter == 0) {
+            throw new HL7Exception("Patient Group is Empty");
+        }
+        for (int i = 0; i < patientCounter; i++) {
+            var orderObsGroup = group.get("PATIENT", i);
+
+            Group patientGroup = (Group) orderObsGroup;
+            var pid = (Segment) patientGroup.get("PID");
+
+            if(pid == null) {
+                throw new HL7Exception("Patient Identifier is Null");
+            }
+
+            String patientName = pid.getField(5, 0).encode();
+
+            if(patientName == null || (patientName != null && patientName.isEmpty())) {
+                throw new HL7Exception("Error Occurred at PID-5");
+            }
+
+        }
+    }
+
+    private static void orderObservationValidator(Group group) throws HL7Exception {
+        var orderCounter = group.getAll("ORDER_OBSERVATION").length;
+        if (orderCounter == 0) {
+            throw new HL7Exception("Order Observation is Empty");
+        }
+
+        for (int i = 0; i < orderCounter; i++) {
+            var orderObsGroup = (Group) group.get("ORDER_OBSERVATION", i);
+            var obr = (Segment) orderObsGroup.get("OBR");
+            if (obr == null) {
+                throw new HL7Exception("OBR is null");
+            }
+
+            String identifierCode = obr.getField(4, 0).encode();
+
+            if(identifierCode == null || (identifierCode != null && identifierCode.isEmpty())) {
+                throw new HL7Exception("Error Occurred at OBR-4");
+            }
+        }
+    }
+}
