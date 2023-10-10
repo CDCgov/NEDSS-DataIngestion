c54f447c4151d2c3a0ed783a05a975caee00aadb
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-10-10 13:02:29 -0700

    clean up

--- report-service/src/main/java/gov/cdc/dataingestion/nbs/ecr/service/helper/CdaProviderMappingHelper.java
+++ report-service/src/main/java/gov/cdc/dataingestion/nbs/ecr/service/helper/CdaProviderMappingHelper.java
@@ -2,6 +2,7 @@ package gov.cdc.dataingestion.nbs.ecr.service.helper;
 
 import gov.cdc.dataingestion.exception.EcrCdaXmlException;
 import gov.cdc.dataingestion.nbs.ecr.model.CdaProviderMapper;
+import gov.cdc.dataingestion.nbs.ecr.model.provider.ProviderFieldCheck;
 import gov.cdc.dataingestion.nbs.ecr.service.helper.interfaces.ICdaMapHelper;
 import gov.cdc.dataingestion.nbs.ecr.service.helper.interfaces.ICdaProviderMappingHelper;
 import gov.cdc.dataingestion.nbs.repository.model.dao.EcrSelectedRecord;
@@ -26,75 +27,20 @@ public class CdaProviderMappingHelper implements ICdaProviderMappingHelper {
 
             if(input.getMsgProviders() != null && !input.getMsgProviders().isEmpty()) {
                 for(int i = 0; i < input.getMsgProviders().size(); i++) {
-                    if (input.getMsgProviders().get(i).getPrvAuthorId() != null
-                            && input.getMsgProviders().get(i).getPrvAuthorId().equalsIgnoreCase(inv168)) {
-                        // ignore
-                    }
-                    else {
-                        int c = 0;
+                    var fieldCheck = mapToProviderTopFieldCheck( input,
+                                     clinicalDocument,
+                                     i,
+                                     inv168,
+                                     performerSectionCounter,
+                                     performerComponentCounter,
+                                     componentCounter);
 
+                    clinicalDocument = fieldCheck.getClinicalDocument();
+                    inv168 = fieldCheck.getInv168();
+                    performerSectionCounter = fieldCheck.getPerformerSectionCounter();
+                    performerComponentCounter = fieldCheck.getPerformerComponentCounter();
+                    componentCounter = fieldCheck.getComponentCounter();
 
-                        if (clinicalDocument.getTitle() == null) {
-                            clinicalDocument.addNewTitle();
-                        }
-
-                        if (clinicalDocument.getCode() == null) {
-                            clinicalDocument.addNewCode();
-                        }
-
-                        if (performerComponentCounter < 1) {
-                            componentCounter++;
-                            performerComponentCounter = componentCounter;
-
-                            var nestedCode = CODE;
-                            if (nestedCode.contains("-")) {
-                                nestedCode = nestedCode.replaceAll("-", ""); // NOSONAR
-                            }
-                            clinicalDocument.getCode().setCode(nestedCode);
-                            clinicalDocument.getCode().setCodeSystem(CLINICAL_CODE_SYSTEM);
-                            clinicalDocument.getCode().setCodeSystemName(CLINICAL_CODE_SYSTEM_NAME);
-                            clinicalDocument.getCode().setDisplayName(CODE_DISPLAY_NAME);
-                            clinicalDocument.getTitle().set(cdaMapHelper.mapToStringData(CLINICAL_TITLE));
-                        }
-
-                        performerSectionCounter = clinicalDocument.getEntryArray().length;
-
-                        if ( clinicalDocument.getEntryArray().length == 0) {
-                            clinicalDocument.addNewEntry();
-                            performerSectionCounter = 0;
-                        }
-                        else {
-                            performerSectionCounter = clinicalDocument.getEntryArray().length;
-                            clinicalDocument.addNewEntry();
-                        }
-
-
-                        if (clinicalDocument.getEntryArray(performerSectionCounter).getAct() == null) {
-                            clinicalDocument.getEntryArray(performerSectionCounter).addNewAct();
-                            clinicalDocument.getEntryArray(performerSectionCounter).getAct().addNewParticipant();
-                        } else {
-                            clinicalDocument.getEntryArray(performerSectionCounter).getAct().addNewParticipant();
-                        }
-                        POCDMT000040Participant2 out = clinicalDocument.getEntryArray(performerSectionCounter).getAct().getParticipantArray(0);
-                        POCDMT000040Participant2 output = this.cdaMapHelper.mapToPSN(
-                                input.getMsgProviders().get(i),
-                                out
-                        );
-                        clinicalDocument.getEntryArray(performerSectionCounter).getAct().setParticipantArray(0, output);
-
-                        clinicalDocument.getEntryArray(performerSectionCounter).setTypeCode(XActRelationshipEntry.COMP);
-                        clinicalDocument.getEntryArray(performerSectionCounter).getAct().setClassCode(XActClassDocumentEntryAct.ACT);
-                        clinicalDocument.getEntryArray(performerSectionCounter).getAct().setMoodCode(XDocumentActMood.EVN);
-
-                        if (clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode() == null){
-                            clinicalDocument.getEntryArray(performerSectionCounter).getAct().addNewCode();
-                        }
-                        clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode().setCode("PSN");
-                        clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode().setCodeSystem(CLINICAL_CODE_SYSTEM);
-                        clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode().setCodeSystemName(CLINICAL_CODE_SYSTEM_NAME);
-                        clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode().setDisplayName(ACT_CODE_DISPLAY_NAME);
-
-                    }
                 }
             }
             mapper.setClinicalSection(clinicalDocument);
@@ -107,5 +53,102 @@ public class CdaProviderMappingHelper implements ICdaProviderMappingHelper {
             throw new EcrCdaXmlException(e.getMessage());
         }
 
+    }
+
+    private POCDMT000040Section mapToProviderTopFieldCheckDocCheck(POCDMT000040Section clinicalDocument) {
+        if (clinicalDocument.getTitle() == null) {
+            clinicalDocument.addNewTitle();
+        }
+
+        if (clinicalDocument.getCode() == null) {
+            clinicalDocument.addNewCode();
+        }
+        return clinicalDocument;
+
+    }
+
+    private POCDMT000040Section mapToProviderTopFieldCheckActCheck(POCDMT000040Section clinicalDocument, int performerSectionCounter) {
+        if (clinicalDocument.getEntryArray(performerSectionCounter).getAct() == null) {
+            clinicalDocument.getEntryArray(performerSectionCounter).addNewAct();
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().addNewParticipant();
+        } else {
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().addNewParticipant();
+        }
+        return clinicalDocument;
+
+    }
+    private ProviderFieldCheck mapToProviderTopFieldCheck(EcrSelectedRecord input,
+                                            POCDMT000040Section clinicalDocument,
+                                            int i,
+                                            String inv168,
+                                            int performerSectionCounter,
+                                            int performerComponentCounter,
+                                            int componentCounter
+                                            ) throws EcrCdaXmlException {
+        if (input.getMsgProviders().get(i).getPrvAuthorId() != null
+                && input.getMsgProviders().get(i).getPrvAuthorId().equalsIgnoreCase(inv168)) {
+            // ignore
+        }
+        else {
+            int c = 0;
+
+            clinicalDocument =  mapToProviderTopFieldCheckDocCheck( clinicalDocument);
+
+            if (performerComponentCounter < 1) {
+                componentCounter++;
+                performerComponentCounter = componentCounter;
+
+                var nestedCode = CODE;
+                if (nestedCode.contains("-")) {
+                    nestedCode = nestedCode.replaceAll("-", ""); // NOSONAR
+                }
+                clinicalDocument.getCode().setCode(nestedCode);
+                clinicalDocument.getCode().setCodeSystem(CLINICAL_CODE_SYSTEM);
+                clinicalDocument.getCode().setCodeSystemName(CLINICAL_CODE_SYSTEM_NAME);
+                clinicalDocument.getCode().setDisplayName(CODE_DISPLAY_NAME);
+                clinicalDocument.getTitle().set(cdaMapHelper.mapToStringData(CLINICAL_TITLE));
+            }
+
+            performerSectionCounter = clinicalDocument.getEntryArray().length;
+
+            if ( clinicalDocument.getEntryArray().length == 0) {
+                clinicalDocument.addNewEntry();
+                performerSectionCounter = 0;
+            }
+            else {
+                performerSectionCounter = clinicalDocument.getEntryArray().length;
+                clinicalDocument.addNewEntry();
+            }
+
+            clinicalDocument = mapToProviderTopFieldCheckActCheck( clinicalDocument,  performerSectionCounter);
+
+            POCDMT000040Participant2 out = clinicalDocument.getEntryArray(performerSectionCounter).getAct().getParticipantArray(0);
+            POCDMT000040Participant2 output = this.cdaMapHelper.mapToPSN(
+                    input.getMsgProviders().get(i),
+                    out
+            );
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().setParticipantArray(0, output);
+
+            clinicalDocument.getEntryArray(performerSectionCounter).setTypeCode(XActRelationshipEntry.COMP);
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().setClassCode(XActClassDocumentEntryAct.ACT);
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().setMoodCode(XDocumentActMood.EVN);
+
+            if (clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode() == null){
+                clinicalDocument.getEntryArray(performerSectionCounter).getAct().addNewCode();
+            }
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode().setCode("PSN");
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode().setCodeSystem(CLINICAL_CODE_SYSTEM);
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode().setCodeSystemName(CLINICAL_CODE_SYSTEM_NAME);
+            clinicalDocument.getEntryArray(performerSectionCounter).getAct().getCode().setDisplayName(ACT_CODE_DISPLAY_NAME);
+
+        }
+
+        ProviderFieldCheck fieldCheck = new ProviderFieldCheck();
+        fieldCheck.setClinicalDocument(clinicalDocument);
+        fieldCheck.setInv168(inv168);
+        fieldCheck.setPerformerSectionCounter(performerSectionCounter);
+        fieldCheck.setPerformerComponentCounter(performerComponentCounter);
+        fieldCheck.setComponentCounter(componentCounter);
+        return fieldCheck;
     }
 }
