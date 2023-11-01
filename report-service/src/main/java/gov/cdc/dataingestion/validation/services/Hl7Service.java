563676d9f75f4966e1e7baa2c8aa3ffc5a129981
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-11-01 11:01:24 -0700

    add hl7 validation service

---
+++ report-service/src/main/java/gov/cdc/dataingestion/validation/services/Hl7Service.java
@@ -0,0 +1,23 @@
+package gov.cdc.dataingestion.validation.services;
+
+import gov.cdc.dataingestion.hl7.helper.integration.exception.DiHL7Exception;
+import gov.cdc.dataingestion.validation.integration.validator.interfaces.IHL7v2Validator;
+import gov.cdc.dataingestion.validation.services.interfaces.IHl7Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Hl7Service implements IHl7Service {
+    private final IHL7v2Validator hl7v2Validator;
+
+    @Autowired
+    public Hl7Service(IHL7v2Validator hl7v2Validator) {
+        this.hl7v2Validator = hl7v2Validator;
+    }
+
+    public String hl7Validator(String message) throws DiHL7Exception {
+        var msg = this.hl7v2Validator.MessageStringValidation(message);
+        msg = this.hl7v2Validator.messageValidation(msg);
+        return msg;
+    }
+}
