95048a405e8441b7358e0fab482af3adfc9b28b1
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2023-11-14 12:53:38 -0700

    unit testing for new code

--- report-service/src/test/java/gov/cdc/dataingestion/nbs/service/NbsRepositoryServiceProviderTest.java
+++ report-service/src/test/java/gov/cdc/dataingestion/nbs/service/NbsRepositoryServiceProviderTest.java
@@ -2,6 +2,10 @@ package gov.cdc.dataingestion.nbs.service;
 
 import gov.cdc.dataingestion.deadletter.repository.model.ElrDeadLetterModel;
 import gov.cdc.dataingestion.hl7.helper.model.HL7ParsedMessage;
+import gov.cdc.dataingestion.hl7.helper.model.hl7.messageGroup.OrderObservation;
+import gov.cdc.dataingestion.hl7.helper.model.hl7.messageGroup.PatientResult;
+import gov.cdc.dataingestion.hl7.helper.model.hl7.messageGroup.Specimen;
+import gov.cdc.dataingestion.hl7.helper.model.hl7.messageType.OruR1;
 import gov.cdc.dataingestion.nbs.TestHelper;
 import gov.cdc.dataingestion.nbs.repository.NbsInterfaceRepository;
 import gov.cdc.dataingestion.nbs.repository.model.NbsInterfaceModel;
@@ -12,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.parameters.P;
 
 import java.io.IOException;
 
@@ -34,12 +39,31 @@ public class NbsRepositoryServiceProviderTest {
         testXmlData = TestHelper.testFileReading();
     }
 
+    @Test
+    void saveToNbsTestNoOruFound() {
+        String id = "whatever";
+        String xmlMsg =  testXmlData;
+        HL7ParsedMessage parsedMessage = new HL7ParsedMessage();
+        when(nbsInterfaceRepo.save(any(NbsInterfaceModel.class))).thenReturn(new NbsInterfaceModel());
+
+        var saved = target.saveXmlMessage(id, xmlMsg, parsedMessage);
+        Assertions.assertTrue(saved instanceof NbsInterfaceModel);
+    }
 
     @Test
     void saveToNbsTest() {
         String id = "whatever";
         String xmlMsg =  testXmlData;
         HL7ParsedMessage parsedMessage = new HL7ParsedMessage();
+        OruR1 oru = new OruR1();
+        oru.getMessageHeader().getSendingFacility().setUniversalId("1");
+        oru.getPatientResult().add(new PatientResult());
+        oru.getPatientResult().get(0).getOrderObservation().add(new OrderObservation());
+        oru.getPatientResult().get(0).getOrderObservation().get(0).getObservationRequest().getFillerOrderNumber().setEntityIdentifier("test");
+        oru.getPatientResult().get(0).getOrderObservation().get(0).getObservationRequest().getUniversalServiceIdentifier().setIdentifier("test");
+        oru.getPatientResult().get(0).getOrderObservation().get(0).getSpecimen().add(new Specimen());
+        oru.getPatientResult().get(0).getOrderObservation().get(0).getSpecimen().get(0).getSpecimen().getSpecimenCollectionDateTime().getRangeStartDateTime().setTime("200603241455");
+        parsedMessage.setParsedMessage(oru);
         when(nbsInterfaceRepo.save(any(NbsInterfaceModel.class))).thenReturn(new NbsInterfaceModel());
 
         var saved = target.saveXmlMessage(id, xmlMsg, parsedMessage);
