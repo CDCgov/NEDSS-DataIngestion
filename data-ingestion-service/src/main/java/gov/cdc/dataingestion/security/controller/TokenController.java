4643a073acda3eece504c7cbd3a1abd0b0ee5eac
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2023-12-19 12:06:50 -0500

    Fixed unit testing.

--- data-ingestion-service/src/main/java/gov/cdc/dataingestion/security/controller/TokenController.java
+++ data-ingestion-service/src/main/java/gov/cdc/dataingestion/security/controller/TokenController.java
@@ -5,7 +5,10 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import gov.cdc.dataingestion.custommetrics.CustomMetricsBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -21,9 +24,14 @@ public class TokenController {
     @Value("${auth.token-uri}")
     String authTokenUri;
     private final CustomMetricsBuilder customMetricsBuilder;
-    private RestTemplate restTemplate= new RestTemplate();
-    public TokenController(CustomMetricsBuilder customMetricsBuilder) {
+   private RestTemplate restTemplate;
+    public TokenController( @Qualifier("restTemplate") RestTemplate restTemplate, CustomMetricsBuilder customMetricsBuilder) {
+        this.restTemplate=restTemplate;
         this.customMetricsBuilder = customMetricsBuilder;
+    }
+    @Bean(name = "restTemplate")
+    public static RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
     @PostMapping("/token")
     public String token(@RequestHeader("client_id") String clientId, @RequestHeader("client_secret") String clientSecret) {
@@ -31,7 +39,6 @@ public class TokenController {
         String postBody = "grant_type=client_credentials" +
                 "&client_id=" + clientId
                 + "&client_secret=" + clientSecret;
-        log.info("Post body : " + postBody);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/x-www-form-urlencoded");
         HttpEntity<String> request = new HttpEntity<>(postBody, headers);
@@ -44,14 +51,10 @@ public class TokenController {
         log.info("Token response status code: " + exchange.getStatusCode());
         String response = exchange.getBody();
         String accessToken = null;
-        try {
             JsonElement jsonElement = JsonParser.parseString(response);
             JsonObject jsonObject = jsonElement.getAsJsonObject();
             accessToken = jsonObject.get("access_token").getAsString();
             customMetricsBuilder.incrementTokensRequested();
-        } catch (Exception e) {
-            log.error("Error in TokenController token method : " + e.getMessage());
-        }
         return accessToken;
     }
 }
--- data-ingestion-service/src/test/java/gov/cdc/dataingestion/security/controller/TokenControllerTest.java
+++ data-ingestion-service/src/test/java/gov/cdc/dataingestion/security/controller/TokenControllerTest.java
@@ -3,26 +3,36 @@ package gov.cdc.dataingestion.security.controller;
 import gov.cdc.dataingestion.custommetrics.CustomMetricsBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.ArgumentMatchers;
-import org.springframework.http.*;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class TokenControllerTest {
     CustomMetricsBuilder customMetricsBuilder;
     RestTemplate restTemplate;
     String expectedToken = "testToken";
+    RestTemplateBuilder templateBuilder;
+
     @BeforeEach
     void setUp() {
         customMetricsBuilder = mock(CustomMetricsBuilder.class);
         restTemplate = mock(RestTemplate.class);
+        templateBuilder=mock(RestTemplateBuilder.class);
     }
     @Test
     void testTokenEndpoint() {
         String authTokenUri = "http://localhost:8080/realms/test/openid-connect/token";
-        TokenController tokenController = new TokenController(customMetricsBuilder);
+        TokenController tokenController = new TokenController(restTemplate,customMetricsBuilder);
         tokenController.authTokenUri = authTokenUri;
         String clientId="test-keycloak-client";
         String clientSecret="testclientsecret";
@@ -39,5 +49,28 @@ class TokenControllerTest {
 
         assertEquals(expectedToken, generatedToken);
         verify(customMetricsBuilder, times(1)).incrementTokensRequested();
+    }
+    @Test
+    void throws_exception_when_invoke_token() {
+        String authTokenUri = "http://localhost:8080/realms/test/openid-connect/token";
+        TokenController tokenController = new TokenController(restTemplate,customMetricsBuilder);
+        tokenController.authTokenUri = authTokenUri;
+        String clientId=null;
+        String clientSecret=null;
+        when(restTemplate.exchange(ArgumentMatchers.anyString(),
+                ArgumentMatchers.any(HttpMethod.class),
+                ArgumentMatchers.any(), ArgumentMatchers.<Class<List<String>>>any()))
+                .thenThrow(HttpClientErrorException.class);
+
+        Executable executable= ()->tokenController.token(clientId,clientSecret);
+        assertThrows(HttpClientErrorException.class,executable);
+    }
+
+    @Test
+    void test_resttemplate_not_null(){
+        RestTemplate restTemplateExpected=new RestTemplate();
+        when(templateBuilder.build()).thenReturn(restTemplateExpected);
+        RestTemplate restTemplateActual= TokenController.restTemplate(templateBuilder);
+        assertNotNull(restTemplateActual);
     }
 }
