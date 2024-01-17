5889cca2c68b4ecb4b5197ea4f383d94bd3cb03b
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-01-17 15:49:23 +0000

    update test

--- data-ingestion-service/src/test/java/gov/cdc/dataingestion/rawmessage/ElrReportsControllerTest.java
+++ data-ingestion-service/src/test/java/gov/cdc/dataingestion/rawmessage/ElrReportsControllerTest.java
@@ -55,7 +55,6 @@ class ElrReportsControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/reports")
                         .param("id", "1").with(SecurityMockMvcRequestPostProcessors.jwt())
                         .header("msgType", messageType)
-                        .header("validationActive", "false")
                         .contentType(MediaType.TEXT_PLAIN_VALUE)
                         .content(payload))
                 .andExpect(MockMvcResultMatchers.status().isOk());
