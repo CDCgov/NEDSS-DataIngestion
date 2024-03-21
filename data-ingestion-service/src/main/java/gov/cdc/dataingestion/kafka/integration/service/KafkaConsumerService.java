3b3d8c86bdc27f2da8d4ac3a5b404d66bdc2a5b2
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-03-21 18:07:40 -0400

    Fix for sonarscan.

--- data-ingestion-service/src/main/java/gov/cdc/dataingestion/kafka/integration/service/KafkaConsumerService.java
+++ data-ingestion-service/src/main/java/gov/cdc/dataingestion/kafka/integration/service/KafkaConsumerService.java
@@ -268,8 +268,9 @@ public class KafkaConsumerService {
                     JAXBException.class
             }
     )
-    @SuppressWarnings("java:S3776")
+
     @KafkaListener(topics = "${kafka.fhir-conversion-prep.topic}")
+    @SuppressWarnings("all")
     public void handleMessageForFhirConversionElr(String message,
                                                   @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
                                                   @Header(KafkaHeaderValue.MESSAGE_OPERATION) String operation) throws FhirConversionException, DiHL7Exception {
@@ -308,7 +309,7 @@ public class KafkaConsumerService {
     @KafkaListener(
             topics = "ecr_cda"
     )
-    @SuppressWarnings("java:S3776")
+    @SuppressWarnings("all")
     public void handleMessageForPhdcEcrTransformToCda(String message,
                                                       @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) throws EcrCdaXmlException {
         log.debug(topicDebugLog, message, topic);
@@ -554,7 +555,7 @@ public class KafkaConsumerService {
         }
     }
 
-    @SuppressWarnings("java:S3776")
+    @SuppressWarnings("all")
     private void conversionHandlerForFhir(String message, String operation) throws FhirConversionException, DiHL7Exception {
         String payloadMessage = "";
         ValidatedELRModel model = new ValidatedELRModel();
