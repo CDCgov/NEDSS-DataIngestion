a31235c55bf723eb4327d8ee479e77d00d286364
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-04-01 13:35:13 -0400

    Added @Param

--- data-ingestion-service/src/main/java/gov/cdc/dataingestion/odse/repository/IEdxActivityLogRepository.java
+++ data-ingestion-service/src/main/java/gov/cdc/dataingestion/odse/repository/IEdxActivityLogRepository.java
@@ -4,6 +4,7 @@ import gov.cdc.dataingestion.odse.repository.model.EdxActivityDetailLog;
 import gov.cdc.dataingestion.odse.repository.model.EdxActivityLogModelView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,8 +14,8 @@ public interface IEdxActivityLogRepository extends JpaRepository<EdxActivityDeta
     @Query(value = """
             select eadl.record_id as recordId ,eadl.record_type as recordType ,eadl.log_type as logType,eadl.log_comment as logComment,
             eal.record_status_time as recordStatusTime from NBS_ODSE.dbo.EDX_activity_detail_log eadl,NBS_ODSE.dbo.EDX_activity_log eal 
-            where eal.source_uid = :sourceId and eadl.edx_activity_log_uid =eal.edx_activity_log_uid
+            where eal.source_uid = :nbsSourceId and eadl.edx_activity_log_uid =eal.edx_activity_log_uid
             """,
             nativeQuery = true)
-    List<EdxActivityLogModelView> getEdxActivityLogDetailsBySourceId(Long sourceId);
+    List<EdxActivityLogModelView> getEdxActivityLogDetailsBySourceId(@Param("nbsSourceId") Long sourceId);
 }
