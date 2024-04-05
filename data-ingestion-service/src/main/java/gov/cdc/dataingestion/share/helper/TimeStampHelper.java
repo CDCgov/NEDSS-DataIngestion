26bfb6f050895d5eabaac20a7a2c3b6298221974
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-04-05 11:08:44 -0400

    Fix for Elr_raw and Elr_dlt showing different timestamps (#146)
    * Fix for Elr_raw and Elr_dlt showing different timestamps
    
    * Update KafkaConsumerService.java
    
    Set timestamp for created and updated time from the DI service.

--- data-ingestion-service/src/main/java/gov/cdc/dataingestion/share/helper/TimeStampHelper.java
+++ data-ingestion-service/src/main/java/gov/cdc/dataingestion/share/helper/TimeStampHelper.java
@@ -2,14 +2,21 @@ package gov.cdc.dataingestion.share.helper;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 public class TimeStampHelper {
     private TimeStampHelper() {
 
     }
     public static Timestamp getCurrentTimeStamp() {
-        // Another Option: Timestamp.from(ZonedDateTime.now().toInstant())
-        return Timestamp.from(Instant.now());
+        // Another Option: Timestamp.from(ZonedDateTime.now().toInstant()) //NOSONAR
+        //return Timestamp.from(Instant.now());//old implementation. //NOSONAR
+        LocalDateTime ldt = LocalDateTime.now();
+        ZonedDateTime zdt = ZonedDateTime.of(ldt, ZoneId.systemDefault());
+        ZonedDateTime gmt = zdt.withZoneSameInstant(ZoneId.of("UTC"));
+        return Timestamp.valueOf(gmt.toLocalDateTime());
     }
     public static Instant getInstantNow() {
         return Instant.now();
