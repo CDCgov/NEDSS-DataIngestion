850c18678b8914d2aeb6cb187430dcbb3fbfcdcf
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-04-18 10:46:24 -0400

    Cndit 1163 rtl setup loggingservice (#153)
    * Logging service initial version
    
    * base version
    
    * Implementation for the Logging service setup

--- data-processing-service/src/main/java/gov/cdc/dataprocessing/service/interfaces/log/IEdxLogService.java
+++ data-processing-service/src/main/java/gov/cdc/dataprocessing/service/interfaces/log/IEdxLogService.java
@@ -1,8 +1,19 @@
 package gov.cdc.dataprocessing.service.interfaces.log;
 
-import gov.cdc.dataprocessing.exception.DataProcessingConsumerException;
 import gov.cdc.dataprocessing.exception.EdxLogException;
+import gov.cdc.dataprocessing.model.dto.log.EDXActivityDetailLogDto;
+import gov.cdc.dataprocessing.model.dto.log.EDXActivityLogDto;
+import gov.cdc.dataprocessing.repository.nbs.odse.model.log.EdxActivityDetailLog;
+import gov.cdc.dataprocessing.repository.nbs.odse.model.log.EdxActivityLog;
 
 public interface IEdxLogService {
     Object processingLog() throws EdxLogException;
+
+    EdxActivityLog saveEdxActivityLog(EDXActivityLogDto edxActivityLogDto) throws EdxLogException;
+
+    EdxActivityDetailLog saveEdxActivityDetailLog(EDXActivityDetailLogDto detailLogDto) throws EdxLogException;
+
+    void saveEdxActivityLogs(String logMessageJson) throws EdxLogException;
+
+    void testKafkaproduceLogMessage();
 }
