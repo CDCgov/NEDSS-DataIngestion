3a29c44ff7732f94cc426d6848c9a19dc5203c55
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-05-15 15:25:12 -0400

    testing wds

---
+++ data-processing-service/src/test/java/gov/cdc/dataprocessing/service/implementation/investigation/DecisionSupportServiceTest.java
@@ -0,0 +1,12 @@
+package gov.cdc.dataprocessing.service.implementation.investigation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DecisionSupportServiceTest {
+
+    @Test
+    void validateProxyContainer() {
+    }
+}
