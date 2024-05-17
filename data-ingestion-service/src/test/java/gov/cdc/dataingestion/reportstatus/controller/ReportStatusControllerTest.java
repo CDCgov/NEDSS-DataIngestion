24afba7bca48c04a51fea321f2810501707037d6
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-05-17 16:43:48 -0400

    Update ReportStatusControllerTest.java
    Removed the unused import.

--- data-ingestion-service/src/test/java/gov/cdc/dataingestion/reportstatus/controller/ReportStatusControllerTest.java
+++ data-ingestion-service/src/test/java/gov/cdc/dataingestion/reportstatus/controller/ReportStatusControllerTest.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ActiveProfiles("test")
