8d81de870c39e8056d7cae8b39f359988cb73473
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-06-04 14:46:27 -0400

    Cndit 1165 test cases for Person, Org, Role (#167)
    * Test cases for the Person service.
    
    * Updated for unit testing.
    
    * code cleanup
    
    * Test cases for OrganizationService
    
    * Unit testing class for the OrganizationMatchingService.
    
    * Update OrganizationMatchingServiceTest.java
    
    test cases for the OrganizationMatchingService
    
    * Create PatientMatchingServiceTest.java
    
    test cases for the PatientMatchingService
    
    * Update OrganizationMatchingService.java
    
    Deleted the debug messages.
    
    * Removed duplicate code.
    
    * test cases for Organization
    
    * Code cleanup.
    
    * Test cases for RoleService
    
    * Added null check
    
    * Update OrganizationRepositoryUtil.java
    
    Code cleanup and updated for unit testing.
    
    * Completed test cases
    
    * Updated test cases.
    
    * Code cleanup
    
    * Update JurisdictionService.java
    
    Added the missing code.
    
    * Test cases for the JurisdictionService.
    
    * Update ProgramAreaCodeRepository.java
    
    Removed unused import.
    
    * Update ProgramAreaService.java
    
    Added null check and NOSONAR for unreachable code.
    
    * Create ProgramAreaServiceTest.java
    
    Test cases for ProgramAreaService.
    
    * Delete PatientMatchingServiceTest.java
    
    * Update ProgramAreaService.java
    
    Rolled back to main version
    
    * Removed unused code.
    
    * Rolled back to main version
    
    * Code cleanup for sonar scan.

--- data-processing-service/src/main/java/gov/cdc/dataprocessing/service/interfaces/organization/IOrganizationService.java
+++ data-processing-service/src/main/java/gov/cdc/dataprocessing/service/interfaces/organization/IOrganizationService.java
@@ -1,11 +1,9 @@
 package gov.cdc.dataprocessing.service.interfaces.organization;
 
 import gov.cdc.dataprocessing.exception.DataProcessingConsumerException;
-import gov.cdc.dataprocessing.exception.DataProcessingException;
-import gov.cdc.dataprocessing.model.container.OrganizationContainer;
 import gov.cdc.dataprocessing.model.container.LabResultProxyContainer;
+import gov.cdc.dataprocessing.model.container.OrganizationContainer;
 
 public interface IOrganizationService {
     OrganizationContainer processingOrganization(LabResultProxyContainer labResultProxyContainer) throws DataProcessingConsumerException;
-    OrganizationContainer testloadObject(long orguid, long actid) throws DataProcessingException;
 }
