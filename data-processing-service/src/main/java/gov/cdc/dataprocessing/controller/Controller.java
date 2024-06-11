bacb389b332ae9386fea945829c6881451689981
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2024-06-11 09:40:36 -0400

    Update Controller.java
    Added status endpoint

--- data-processing-service/src/main/java/gov/cdc/dataprocessing/controller/Controller.java
+++ data-processing-service/src/main/java/gov/cdc/dataprocessing/controller/Controller.java
@@ -4,13 +4,16 @@ import gov.cdc.dataprocessing.exception.DataProcessingException;
 import gov.cdc.dataprocessing.service.implementation.manager.ManagerService;
 import gov.cdc.dataprocessing.service.interfaces.manager.IManagerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/data-processing-svc/rti")
 @RequiredArgsConstructor
+@Slf4j
 public class Controller {
     private final IManagerService managerService;
     @Autowired
@@ -27,5 +30,11 @@ public class Controller {
             throw  new DataProcessingException(e.getMessage());
         }
         return ResponseEntity.ok("OK");
+    }
+    @GetMapping("/status")
+    @ResponseBody
+    public ResponseEntity<String> getDataPipelineStatusHealth() {
+        log.info("Data Processing Service Status OK");
+        return ResponseEntity.status(HttpStatus.OK).body("Data Processing Service Status OK");
     }
 }
