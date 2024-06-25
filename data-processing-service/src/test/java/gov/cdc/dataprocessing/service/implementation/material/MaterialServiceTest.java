76f9e5d3e4f655767fd57c212ed567cdc454637f
Alaap Bharadwaj <abharadwaj@enquizit.com>
2024-06-25 10:42:19 -0400

    material service testing

---
+++ data-processing-service/src/test/java/gov/cdc/dataprocessing/service/implementation/material/MaterialServiceTest.java
@@ -0,0 +1,16 @@
+package gov.cdc.dataprocessing.service.implementation.material;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaterialServiceTest {
+
+    @Test
+    void loadMaterialObject() {
+    }
+
+    @Test
+    void saveMaterial() {
+    }
+}
