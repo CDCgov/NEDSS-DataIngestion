97dd9a356b82b0250ec1ff69bbe3b4669537d94f
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-07-13 20:06:16 -0400

    up

---
+++ data-processing-service/src/test/java/gov/cdc/dataprocessing/utilities/component/data_parser/ObsReqNoteHelperTest.java
@@ -0,0 +1,79 @@
+package gov.cdc.dataprocessing.utilities.component.data_parser;
+
+import gov.cdc.dataprocessing.constant.elr.EdxELRConstant;
+import gov.cdc.dataprocessing.exception.DataProcessingException;
+import gov.cdc.dataprocessing.model.container.model.ObservationContainer;
+import gov.cdc.dataprocessing.model.dto.observation.ObsValueTxtDto;
+import gov.cdc.dataprocessing.model.dto.observation.ObservationDto;
+import gov.cdc.dataprocessing.model.phdc.HL7NTEType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+class ObsReqNoteHelperTest {
+
+    private ObservationContainer observationContainer;
+    private HL7NTEType hl7NTEType;
+
+    @BeforeEach
+    void setUp() {
+        observationContainer = mock(ObservationContainer.class);
+        hl7NTEType = mock(HL7NTEType.class);
+    }
+
+    @Test
+    void testGetObsReqNotes_WithComments() throws DataProcessingException {
+        List<String> comments = new ArrayList<>();
+        comments.add("Test Comment 1");
+        comments.add("Test Comment 2");
+
+        when(hl7NTEType.getHL7Comment()).thenReturn(comments);
+        when(observationContainer.getTheObsValueTxtDtoCollection()).thenReturn(new ArrayList<>());
+        when(observationContainer.getTheObservationDto()).thenReturn(new ObservationDto());
+
+        List<HL7NTEType> noteArray = new ArrayList<>();
+        noteArray.add(hl7NTEType);
+
+        ObservationContainer result = ObsReqNoteHelper.getObsReqNotes(noteArray, observationContainer);
+
+        assertNotNull(result);
+        assertEquals(2, result.getTheObsValueTxtDtoCollection().size());
+        verify(observationContainer, times(7)).getTheObsValueTxtDtoCollection();
+    }
+
+    @Test
+    void testGetObsReqNotes_WithoutComments() throws DataProcessingException {
+        when(hl7NTEType.getHL7Comment()).thenReturn(new ArrayList<>());
+        when(observationContainer.getTheObsValueTxtDtoCollection()).thenReturn(new ArrayList<>());
+        when(observationContainer.getTheObservationDto()).thenReturn(new ObservationDto());
+
+        List<HL7NTEType> noteArray = new ArrayList<>();
+        noteArray.add(hl7NTEType);
+
+        ObservationContainer result = ObsReqNoteHelper.getObsReqNotes(noteArray, observationContainer);
+
+        assertNotNull(result);
+        assertEquals(1, result.getTheObsValueTxtDtoCollection().size());
+        verify(observationContainer, times(4)).getTheObsValueTxtDtoCollection();
+    }
+
+    @Test
+    void testGetObsReqNotes_Exception() {
+        List<HL7NTEType> noteArray = new ArrayList<>();
+        noteArray.add(hl7NTEType);
+
+        try {
+            doThrow(new RuntimeException("Test Exception")).when(hl7NTEType).getHL7Comment();
+            ObsReqNoteHelper.getObsReqNotes(noteArray, observationContainer);
+        } catch (DataProcessingException e) {
+            assertEquals("Exception thrown at ObservationResultRequest.getObsReqNotes:Test Exception", e.getMessage());
+        }
+    }
+}
---
+++ data-processing-service/src/test/java/gov/cdc/dataprocessing/utilities/component/wds/WdsObjectCheckerTest.java
@@ -0,0 +1,150 @@
+package gov.cdc.dataprocessing.utilities.component.wds;
+
+import gov.cdc.dataprocessing.constant.elr.NEDSSConstant;
+import gov.cdc.dataprocessing.model.dto.edx.EdxRuleManageDto;
+import gov.cdc.dataprocessing.model.dto.nbs.NbsQuestionMetadata;
+import gov.cdc.dataprocessing.utilities.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import java.lang.reflect.Method;
+import java.sql.Timestamp;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class WdsObjectCheckerTest {
+
+    @InjectMocks
+    private WdsObjectChecker wdsObjectChecker;
+
+    private EdxRuleManageDto edxRuleManageDto;
+    private NbsQuestionMetadata metaData;
+    private TestObject testObject;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        edxRuleManageDto = mock(EdxRuleManageDto.class);
+        metaData = mock(NbsQuestionMetadata.class);
+        testObject = new TestObject();
+    }
+
+    @Test
+    void testCheckNbsObject_TextEquals() {
+        testObject.setSampleText("testValue");
+        when(metaData.getDataLocation()).thenReturn("testObject.sampleText");
+        when(metaData.getDataType()).thenReturn(NEDSSConstant.NBS_QUESTION_DATATYPE_TEXT);
+        when(edxRuleManageDto.getLogic()).thenReturn("=");
+        when(edxRuleManageDto.getValue()).thenReturn("testValue");
+
+        boolean result = wdsObjectChecker.checkNbsObject(edxRuleManageDto, testObject, metaData);
+        assertTrue(result);
+    }
+
+    @Test
+    void testCheckNbsObject_TextNotEquals() {
+        testObject.setSampleText("testValue");
+        when(metaData.getDataLocation()).thenReturn("testObject.sampleText");
+        when(metaData.getDataType()).thenReturn(NEDSSConstant.NBS_QUESTION_DATATYPE_TEXT);
+        when(edxRuleManageDto.getLogic()).thenReturn("!=");
+        when(edxRuleManageDto.getValue()).thenReturn("differentValue");
+
+        boolean result = wdsObjectChecker.checkNbsObject(edxRuleManageDto, testObject, metaData);
+        assertTrue(result);
+    }
+
+    @Test
+    void testCheckNbsObject_DateEquals() throws Exception {
+        Timestamp timestamp = Timestamp.valueOf("2022-12-31 00:00:00.0");
+        testObject.setSampleTimestamp(timestamp);
+        when(metaData.getDataLocation()).thenReturn("testObject.sampleTimestamp");
+        when(metaData.getDataType()).thenReturn(NEDSSConstant.NBS_QUESTION_DATATYPE_DATE);
+        when(edxRuleManageDto.getLogic()).thenReturn("=");
+        when(edxRuleManageDto.getValue()).thenReturn("12/31/2022 00:00:00");
+
+        boolean result = wdsObjectChecker.checkNbsObject(edxRuleManageDto, testObject, metaData);
+        assertTrue(result);
+    }
+
+    @Test
+    void testCheckNbsObject_DateNotEquals() throws Exception {
+        Timestamp timestamp = Timestamp.valueOf("2022-12-31 00:00:00.0");
+        testObject.setSampleTimestamp(timestamp);
+        when(metaData.getDataLocation()).thenReturn("testObject.sampleTimestamp");
+        when(metaData.getDataType()).thenReturn(NEDSSConstant.NBS_QUESTION_DATATYPE_DATE);
+        when(edxRuleManageDto.getLogic()).thenReturn("!=");
+        when(edxRuleManageDto.getValue()).thenReturn("01/01/2023 00:00:00");
+
+        boolean result = wdsObjectChecker.checkNbsObject(edxRuleManageDto, testObject, metaData);
+        assertTrue(result);
+    }
+
+    @Test
+    void testCheckNbsObject_NumericGreaterThan() {
+        testObject.setSampleNumber(10L);
+        when(metaData.getDataLocation()).thenReturn("testObject.sampleNumber");
+        when(metaData.getDataType()).thenReturn(NEDSSConstant.NBS_QUESTION_DATATYPE_NUMERIC);
+        when(edxRuleManageDto.getLogic()).thenReturn(">");
+        when(edxRuleManageDto.getValue()).thenReturn("5");
+
+        boolean result = wdsObjectChecker.checkNbsObject(edxRuleManageDto, testObject, metaData);
+        assertTrue(result);
+    }
+
+    @Test
+    void testCheckNbsObject_NumericLessThan() {
+        testObject.setSampleNumber(3L);
+        when(metaData.getDataLocation()).thenReturn("testObject.sampleNumber");
+        when(metaData.getDataType()).thenReturn(NEDSSConstant.NBS_QUESTION_DATATYPE_NUMERIC);
+        when(edxRuleManageDto.getLogic()).thenReturn("<");
+        when(edxRuleManageDto.getValue()).thenReturn("5");
+
+        boolean result = wdsObjectChecker.checkNbsObject(edxRuleManageDto, testObject, metaData);
+        assertTrue(result);
+    }
+
+    @Test
+    void testCheckNbsObject_InvalidMethod() {
+        when(metaData.getDataLocation()).thenReturn("testObject.invalidField");
+        when(metaData.getDataType()).thenReturn(NEDSSConstant.NBS_QUESTION_DATATYPE_TEXT);
+        when(edxRuleManageDto.getLogic()).thenReturn("=");
+        when(edxRuleManageDto.getValue()).thenReturn("value");
+
+        boolean result = wdsObjectChecker.checkNbsObject(edxRuleManageDto, testObject, metaData);
+        assertFalse(result);
+    }
+
+    // Test object for method invocation
+    private static class TestObject {
+        private String sampleText;
+        private Timestamp sampleTimestamp;
+        private Long sampleNumber;
+
+        public String getSampleText() {
+            return sampleText;
+        }
+
+        public void setSampleText(String sampleText) {
+            this.sampleText = sampleText;
+        }
+
+        public Timestamp getSampleTimestamp() {
+            return sampleTimestamp;
+        }
+
+        public void setSampleTimestamp(Timestamp sampleTimestamp) {
+            this.sampleTimestamp = sampleTimestamp;
+        }
+
+        public Long getSampleNumber() {
+            return sampleNumber;
+        }
+
+        public void setSampleNumber(Long sampleNumber) {
+            this.sampleNumber = sampleNumber;
+        }
+    }
+}
