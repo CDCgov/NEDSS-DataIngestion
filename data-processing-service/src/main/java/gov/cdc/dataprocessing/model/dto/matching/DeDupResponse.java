8a2f44d9309a8cb08d85cb1af0c00d05038624b3
Sheham Izat <sizat@enquizit.com>
2024-08-26 09:16:04 -0400

    Create De-Dup Response object to send it to Person Service

---
+++ data-processing-service/src/main/java/gov/cdc/dataprocessing/model/dto/matching/DeDupResponse.java
@@ -0,0 +1,44 @@
+package gov.cdc.dataprocessing.model.dto.matching;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class DeDupResponse {
+
+
+  boolean matchFound;
+  MatchType matchType;
+
+  public DeDupResponse(boolean matchFound, boolean needHumanReview) {
+    this.matchFound = matchFound;
+    if (!matchFound) {
+      matchType = MatchType.NO_MATCH;
+    }
+    if (matchFound && needHumanReview) {
+      matchType = MatchType.MATCH_MANUAL_REVIEW;
+    }
+    if (matchFound && !needHumanReview) {
+      matchType = MatchType.MATCH_AUTO_MERGE;
+    }
+  }
+
+  public enum MatchType {
+    NO_MATCH("No match"),
+    MATCH_MANUAL_REVIEW("Match and it needs to go thru Manual review"),
+    MATCH_AUTO_MERGE("Match and it can be Auto merged");
+
+    private final String description;
+
+    MatchType(String description) {
+      this.description = description;
+    }
+
+    @Override
+    public String toString() {
+      return description;
+    }
+  }
+
+}
