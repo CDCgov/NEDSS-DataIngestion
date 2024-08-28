1c8450011e996526b7cedfaf0085015c37857dd2
Ragul Shanmugam <123001929+rshanmugam-enquizit@users.noreply.github.com>
2024-08-28 10:32:40 -0700

    Poll service added for ECRs and endpoint updated based on Rhapsody route (#211)

--- data-ingestion-service/src/main/java/gov/cdc/dataingestion/rawmessage/controller/EcrReportsController.java
+++ data-ingestion-service/src/main/java/gov/cdc/dataingestion/rawmessage/controller/EcrReportsController.java
@@ -1,9 +1,7 @@
 package gov.cdc.dataingestion.rawmessage.controller;
 
-import gov.cdc.dataingestion.nbs.ecr.service.interfaces.ICdaMapper;
 import gov.cdc.dataingestion.nbs.repository.model.NbsInterfaceModel;
 import gov.cdc.dataingestion.nbs.services.NbsRepositoryServiceProvider;
-import gov.cdc.dataingestion.nbs.services.interfaces.IEcrMsgQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -12,32 +10,30 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequiredArgsConstructor
 @SecurityRequirement(name = "bearer-key")
 @Tag(name = "ECR Ingestion", description = "ECR Ingestion API")
 public class EcrReportsController {
-    private IEcrMsgQueryService ecrMsgQueryService;
-    private ICdaMapper cdaMapper;
 
     private NbsRepositoryServiceProvider nbsRepositoryServiceProvider;
 
     @Autowired
-    public EcrReportsController(IEcrMsgQueryService ecrMsgQueryService, ICdaMapper cdaMapper, NbsRepositoryServiceProvider nbsRepositoryServiceProvider) {
-        this.ecrMsgQueryService = ecrMsgQueryService;
-        this.cdaMapper = cdaMapper;
+    public EcrReportsController(NbsRepositoryServiceProvider nbsRepositoryServiceProvider) {
         this.nbsRepositoryServiceProvider = nbsRepositoryServiceProvider;
     }
 
     @Operation(
-            summary = "Submit a ECR document to Data Ingestion Service",
+            summary = "Submit an ECR document to Data Ingestion Service",
             parameters = {
                     @Parameter(in = ParameterIn.HEADER,
                             name = "clientid",
@@ -52,17 +48,32 @@ public class EcrReportsController {
     )
     @PostMapping(consumes = MediaType.TEXT_PLAIN_VALUE, path = "/api/ecrs")
     public ResponseEntity<Integer> saveIncomingEcr(@RequestBody final String payload,
-                                                   @RequestHeader("rr-message") String incomingRR,
-                                                   @RequestHeader("system-nm") String systemNm,
-                                                   @RequestHeader("orig-doc-type-eicr") String origDocTypeEicr,
-                                                   @RequestHeader("orig-doc-type-rr") String origDocTypeRR) {
-        NbsInterfaceModel nbsInterfaceModel;
-        if(incomingRR.equalsIgnoreCase("null") || incomingRR.isEmpty() || incomingRR.isBlank()) {
-            nbsInterfaceModel = nbsRepositoryServiceProvider.saveIncomingEcrMessageWithoutRR(payload, systemNm, origDocTypeEicr);
+                                                   @RequestHeader("systemNm") String systemNm,
+                                                   @RequestHeader("origDocTypeEicr") String origDocTypeEicr,
+                                                   @RequestHeader("origDocTypeRR, required = false") String origDocTypeRR) {
+        try {
+            String eicrXml = extractXmlContent(payload, "<eICRXML>", "</eICRXML>");
+            String rrXml = extractXmlContent(payload, "<RRXML>", "</RRXML>");
+
+            NbsInterfaceModel nbsInterfaceModel;
+            if(rrXml.equalsIgnoreCase("null") || rrXml.isEmpty() || rrXml.isBlank()) {
+                nbsInterfaceModel = nbsRepositoryServiceProvider.saveIncomingEcrMessageWithoutRR(eicrXml, systemNm, origDocTypeEicr);
+            }
+            else {
+                nbsInterfaceModel = nbsRepositoryServiceProvider.saveIncomingEcrMessageWithRR(eicrXml, systemNm, origDocTypeEicr, rrXml, origDocTypeRR);
+            }
+            return ResponseEntity.ok(nbsInterfaceModel.getNbsInterfaceUid());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Something went wrong while parsing the payload", e);
         }
-        else {
-            nbsInterfaceModel = nbsRepositoryServiceProvider.saveIncomingEcrMessageWithRR(payload, systemNm, origDocTypeEicr, incomingRR, origDocTypeRR);
+    }
+
+    private String extractXmlContent(String textInput, String startTag, String endTag) {
+        int startIndex = textInput.indexOf(startTag) + startTag.length();
+        int endIndex = textInput.indexOf(endTag);
+        if (startIndex < 0 || endIndex < 0 || startIndex >= endIndex) {
+            throw new IllegalArgumentException("XML tags not found or malformed input");
         }
-        return ResponseEntity.ok(nbsInterfaceModel.getNbsInterfaceUid());
+        return textInput.substring(startIndex, endIndex).trim();
     }
 }
