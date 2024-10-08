ce5b38e192ccb6a6afe20aaefb350358435650f8
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-10-08 14:51:00 -0400

    clean up

--- data-ingestion-service/src/main/java/gov/cdc/dataingestion/reportstatus/controller/ReportStatusController.java
+++ data-ingestion-service/src/main/java/gov/cdc/dataingestion/reportstatus/controller/ReportStatusController.java
@@ -22,7 +22,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @SecurityRequirement(name = "bearer-key")
