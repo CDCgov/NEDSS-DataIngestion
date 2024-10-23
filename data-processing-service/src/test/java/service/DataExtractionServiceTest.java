23b2fbfab5afb62ecba9121c49e3598d64cfb738
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-10-23 19:05:42 -0400

    commented out faulted code

--- data-processing-service/src/test/java/service/DataExtractionServiceTest.java
+++ data-processing-service/src/test/java/service/DataExtractionServiceTest.java
@@ -1,15 +1,14 @@
 package service;
 
 //
-///**
- 125 - Comment complaint
- 3776 - Complex complaint
- 6204 - Forcing convert to stream to list complaint
- 1141 - Nested complaint
- 6809 - TEST
- */
-@SuppressWarnings({"java:S125", "java:S3776", "java:S6204", "java:S1141", "java:S6809"})
-public class DataExtractionServiceTest {
+// 125 - Comment complaint
+// 3776 - Complex complaint
+// 6204 - Forcing convert to stream to list complaint
+// 1141 - Nested complaint
+// 6809 - TEST
+
+//VOIDED @SuppressWarnings({"java:S125", "java:S3776", "java:S6204", "java:S1141", "java:S6809"})
+//VOIDED public class DataExtractionServiceTest {
 //
 //    @InjectMocks
 //    private DataExtractionService dataExtractionService;
