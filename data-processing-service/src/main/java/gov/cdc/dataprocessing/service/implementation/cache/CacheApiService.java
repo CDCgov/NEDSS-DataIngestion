7ebd45dbe70ee39e86fdd8d2312bba1a7f9f972e
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2024-12-06 17:20:22 -0500

    small ketchup ready for deploy

--- data-processing-service/src/main/java/gov/cdc/dataprocessing/service/implementation/cache/CacheApiService.java
+++ data-processing-service/src/main/java/gov/cdc/dataprocessing/service/implementation/cache/CacheApiService.java
@@ -4,7 +4,7 @@ package gov.cdc.dataprocessing.service.implementation.cache;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import gov.cdc.dataprocessing.service.interfaces.cache.ICacheApiService;
-import org.apache.kafka.common.protocol.types.Field;
+import gov.cdc.dataprocessing.service.interfaces.cache.ITokenService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -22,11 +22,6 @@ import java.util.Map;
 
 @Service
 public class CacheApiService implements ICacheApiService {
-    @Value("${cache.clientId}")
-    private String clientId = "clientId";
-
-    @Value("${cache.secret}")
-    private String clientSecret = "clientSecret";
 
     @Value("${cache.srte.cacheString}")
     protected String srteCacheString;
@@ -40,13 +35,20 @@ public class CacheApiService implements ICacheApiService {
     @Value("${cache.odse.localId}")
     protected String odseLocalId;
 
+    @Value("${cache.clientId}")
+    private String clientId;
 
+    @Value("${cache.secret}")
+    private String clientSecret;
+
+    private final ITokenService tokenService;
 
     private final Gson gson;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    public CacheApiService() {
+    public CacheApiService(ITokenService tokenService) {
+        this.tokenService = tokenService;
         this.gson = new GsonBuilder()
                 .setDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
                 .create();
@@ -54,32 +56,36 @@ public class CacheApiService implements ICacheApiService {
 
     public String getSrteCacheString(String objectName, String key) {
         var param = new HashMap<String, String>();
+
         param.put("key", key);
-        return callEndpoint(srteCacheString + "/" + objectName, param, "token", String.class);
+        return callEndpoint(srteCacheString + "/" + objectName, param, tokenService.getToken(), String.class);
     }
 
     public String getSrteCacheObject(String objectName, String key) {
         var param = new HashMap<String, String>();
         param.put("key", key);
-        return callEndpoint(srteCacheObject + "/" + objectName, param, "token", String.class);
+        return callEndpoint(srteCacheObject + "/" + objectName, param, tokenService.getToken(), String.class);
     }
 
     public Boolean getSrteCacheBool(String objectName, String key) {
         var param = new HashMap<String, String>();
         param.put("key", key);
-        return callEndpoint(srteCacheContain + "/" + objectName, param, "token", Boolean.class);
+        return callEndpoint(srteCacheContain + "/" + objectName, param, tokenService.getToken(), Boolean.class);
     }
 
     public String getOdseLocalId(String objectName, boolean geApplied) {
         var param = new HashMap<String, String>();
         param.put("localIdClass", objectName);
         param.put("geApplied", String.valueOf(geApplied));
-        return callEndpoint(odseLocalId, param, "token", String.class);
+        return callEndpoint(odseLocalId, param, tokenService.getToken(), String.class);
     }
 
     protected  <T> T callEndpoint(String endpoint, Map<String, String> params, String token, Class<T> responseType) {
         HttpHeaders headers = new HttpHeaders();
-       // headers.setBearerAuth(token);
+        headers.setBearerAuth(token);
+        headers.add("clientid", clientId);
+        headers.add("clientsecret", clientSecret);
+
         HttpEntity<String> entity = new HttpEntity<>(headers);
 
         MultiValueMap<String, String> multiValueParams = new LinkedMultiValueMap<>();
--- rti-cache/src/main/java/gov/cdc/rticache/controller/Controller.java
+++ rti-cache/src/main/java/gov/cdc/rticache/controller/Controller.java
@@ -3,9 +3,12 @@ package gov.cdc.rticache.controller;
 import gov.cdc.rticache.constant.LocalIdClass;
 import gov.cdc.rticache.constant.ObjectName;
 import gov.cdc.rticache.exception.RtiCacheException;
+import gov.cdc.rticache.service.OdseIdGeneratorWCacheService;
 import gov.cdc.rticache.service.interfaces.IManagerCacheService;
 import gov.cdc.rticache.service.interfaces.IOdseIdGeneratorWCacheService;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 public class Controller {
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class); // NOSONAR
+
     private final IManagerCacheService managerCacheService;
     private final IOdseIdGeneratorWCacheService odseIdGeneratorWCacheService;
 
@@ -32,29 +37,29 @@ public class Controller {
     @GetMapping(path = "/srte/cache/string/{objectName}")
     public ResponseEntity<String> getSrteCacheMapString(@PathVariable String objectName, @RequestParam String key) throws RtiCacheException {
         var res =  managerCacheService.getCache(ObjectName.valueOf(objectName), key);
+        logger.info("/srte/cache/string/{}?key={}", objectName, key);
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
     @GetMapping(path = "/srte/cache/object/{objectName}")
     public ResponseEntity<Object> getSrteCacheObject(@PathVariable String objectName, @RequestParam String key) throws RtiCacheException {
         var res =  managerCacheService.getCacheObject(ObjectName.valueOf(objectName), key);
+        logger.info("/srte/cache/object/{}?key={}", objectName, key);
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
     @GetMapping(path = "/srte/cache/contain/{objectName}")
     public ResponseEntity<Boolean> getSrteCacheMapContain(@PathVariable String objectName, @RequestParam String key) throws RtiCacheException {
         var res =  managerCacheService.containKey(ObjectName.valueOf(objectName), key);
+        logger.info("/srte/cache/contain/{}?key={}", objectName, key);
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
     @GetMapping(path = "/odse/localId")
     public ResponseEntity<Object> getOdseLocalId(@RequestParam String localIdClass, @RequestParam boolean geApplied) throws RtiCacheException {
         var res = odseIdGeneratorWCacheService.getValidLocalUid(LocalIdClass.valueOf(localIdClass), geApplied);
+        logger.info("/odse/localId/{}?geApplied={}", localIdClass, geApplied);
         return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
-    @GetMapping(path = "/odse/cache")
-    public ResponseEntity<String> getOdseCache() throws RtiCacheException {
-        return ResponseEntity.status(HttpStatus.OK).body("Data Processing Service Status OK");
-    }
 }
