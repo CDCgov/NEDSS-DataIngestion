7bc412d4334bad3c4c530b5ebecef619bd52ee57
Michael Peels <109251240+mpeels@users.noreply.github.com>
2024-12-13 12:30:43 -0500

    Workflow update (#252)
    * Seed process in deduplication service
    
    * Some unit testing. Resolve code smell?
    
    * Placeholder tests
    
    * Use test containers for nbs and mpi db
    
    * Fix tab v spacing in build.gradle
    
    * Use restored  MSSQL test container database for testing
    
    * Remove merge duplicate
    
    * Correct parameter
    
    * EOF newlines
    
    * Fix property name
    
    * Run build on srte-data-service or phdc change
    
    ---------
    
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels.local>

--- deduplication/src/test/java/gov/cdc/nbs/deduplication/config/container/PostgresContainer.java
+++ deduplication/src/test/java/gov/cdc/nbs/deduplication/config/container/PostgresContainer.java
@@ -1,4 +1,4 @@
-package gov.cdc.nbs.deduplication.config;
+package gov.cdc.nbs.deduplication.config.container;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
