097abdad40354538cf894cd6a3526b2edbf00f71
Michael Peels <109251240+mpeels@users.noreply.github.com>
2024-12-19 13:51:46 -0500

    Use ObjectProvider for service dependency (#261)
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels.local>

--- data-processing-service/src/main/java/gov/cdc/dataprocessing/service/implementation/person/matching/DeduplicationClient.java
+++ data-processing-service/src/main/java/gov/cdc/dataprocessing/service/implementation/person/matching/DeduplicationClient.java
@@ -1,15 +1,19 @@
 package gov.cdc.dataprocessing.service.implementation.person.matching;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
+@ConditionalOnProperty(name = "${features.modernizedMatching.enabled}", havingValue = "true")
 public class DeduplicationClient {
 
-  @Bean("deduplicationRestClient")
-  public RestClient restClient(@Value("${features.modernizedMatching.url}") String modernizedMatchingUrl) {
-    return RestClient.builder().baseUrl(modernizedMatchingUrl).build();
+  @Bean
+  public DeduplicationService deduplicationService(
+      @Value("${features.modernizedMatching.url}") String modernizedMatchingUrl) {
+    return new DeduplicationService(RestClient.builder().baseUrl(modernizedMatchingUrl).build());
   }
+
 }
