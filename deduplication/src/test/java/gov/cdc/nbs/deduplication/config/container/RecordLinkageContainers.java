69f8043d06ad4d6c7255f34b6747cb54e47d231a
Michael Peels <michaelpeels@Michael-Peels.local>
2024-12-19 16:21:00 -0500

    Add RL and postgres container

--- deduplication/src/test/java/gov/cdc/nbs/deduplication/config/container/RecordLinkageContainers.java
+++ deduplication/src/test/java/gov/cdc/nbs/deduplication/config/container/RecordLinkageContainers.java
@@ -2,15 +2,29 @@ package gov.cdc.nbs.deduplication.config.container;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @Configuration
 @Profile("test")
-public class PostgresContainer {
+public class RecordLinkageContainers {
+
+  private static Network network = Network.newNetwork();
 
   @SuppressWarnings("resource") // We don't want to close this
   private static final PostgreSQLContainer<?> POSTGRES_CONTAINER = new PostgreSQLContainer<>("postgres")
-      .withInitScript("postgres.sql");
+      .withInitScript("postgres.sql")
+      .withNetwork(network)
+      .withNetworkAliases("postgres");
+
+  @SuppressWarnings("resource") // We don't want to close this
+  private static final GenericContainer<?> RL_CONTAINER = new GenericContainer<>(
+      "ghcr.io/cdcgov/recordlinker:latest")
+      .withExposedPorts(8080)
+      .withNetwork(network)
+      .withNetworkAliases("recordLinkage")
+      .withEnv("DB_URI", "postgresql+psycopg2://test:test@postgres:5432/postgres");
 
   static {
     POSTGRES_CONTAINER.start();
@@ -19,6 +33,9 @@ public class PostgresContainer {
     System.setProperty("spring.datasource.mpi.url", POSTGRES_CONTAINER.getJdbcUrl());
     System.setProperty("spring.datasource.mpi.username", POSTGRES_CONTAINER.getUsername());
     System.setProperty("spring.datasource.mpi.password", POSTGRES_CONTAINER.getPassword());
+
+    RL_CONTAINER.start();
+    System.setProperty("recordLinkage.url", "http://localhost:" + RL_CONTAINER.getMappedPort(8080));
   }
 
 }
