4cb836d68fa672ec1df73e423dcab1fd08761455
Michael Peels <michaelpeels@Michael-Peels.local>
2025-01-07 16:05:25 -0500

    Clean up file read

--- data-ingestion-service/src/test/java/gov/cdc/dataingestion/nbs/TestHelper.java
+++ data-ingestion-service/src/test/java/gov/cdc/dataingestion/nbs/TestHelper.java
@@ -1,34 +1,14 @@
 package gov.cdc.dataingestion.nbs;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-/**
- 1118 - require constructor complaint
- 125 - comment complaint
- 6126 - String block complaint
- 1135 - todos complaint
- * */
-@SuppressWarnings({"java:S1118","java:S125", "java:S6126", "java:S1135"})
 public class TestHelper {
 
     public static String testFileReading() throws IOException {
-        var file = new File("src/test/resources/xmlData.txt").getAbsolutePath();
-
-        String result = "";
-        try (FileReader fileReader = new FileReader(file);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            String line = "";
-            while ((line = bufferedReader.readLine()) != null) {
-                result += line;
-            }
-
-            return result;
-
-        } catch (IOException e) {
-            throw new IOException("An error occurred while reading the file: " + e.getMessage());
-        }
+        String path = new File("src/test/resources/xmlData.txt").getAbsolutePath();
+        return Files.readString(Paths.get(path));
     }
 }
