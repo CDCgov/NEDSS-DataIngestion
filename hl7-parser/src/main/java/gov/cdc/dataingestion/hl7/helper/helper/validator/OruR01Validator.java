c1dbf35d5140bacc3ab68b3bef44743be188cd8a
Selvarasu Sathiah <140747625+ssathiah@users.noreply.github.com>
2025-01-09 11:54:16 -0500

    Updated Unit testing and Sonar scan fix

--- hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/helper/validator/OruR01Validator.java
+++ hl7-parser/src/main/java/gov/cdc/dataingestion/hl7/helper/helper/validator/OruR01Validator.java
@@ -7,7 +7,11 @@ import ca.uhn.hl7v2.model.Segment;
 
 public class OruR01Validator {
     private static final String REQ_FLD_MISSING = "Required field missing ";
-    private static final String ORDER_OBSERVATION ="ORDER_OBSERVATION";
+    private static final String PATIENT_RESULT = "PATIENT_RESULT";
+    private static final String ORDER_OBSERVATION = "ORDER_OBSERVATION";
+    private static final String OBSERVATION = "OBSERVATION";
+    private static final String PATIENT = "PATIENT";
+    private static final String SPECIMEN = "SPECIMEN";
 
     private OruR01Validator() {
 
@@ -16,7 +20,7 @@ public class OruR01Validator {
     public static void patientResultValidator(Group group, Location location) throws HL7Exception {
         var groupName = group.getName();
         var groupNameWithLocation = location.toString();
-        if (groupName.equalsIgnoreCase("PATIENT_RESULT")) {
+        if (groupName.equalsIgnoreCase(PATIENT_RESULT)) {
             /// PATIENT
             patientValidator(group, groupNameWithLocation);
             // ORDER OBSERVATION
@@ -27,17 +31,17 @@ public class OruR01Validator {
     public static void mshValidator(Segment segment) throws HL7Exception {
         String dateTime = segment.getField(7, 0).encode();
         if (dateTime == null || dateTime.isEmpty()) {
-            throw new HL7Exception(REQ_FLD_MISSING+"/MSH/DateTimeOfMessage/Time/Year");
+            throw new HL7Exception(REQ_FLD_MISSING + "/MSH/DateTimeOfMessage/Time/Year");
         }
 
         String msgControlId = segment.getField(10, 0).encode();
         if (msgControlId == null || msgControlId.isEmpty()) {
-            throw new HL7Exception(REQ_FLD_MISSING+"/MSH/MessageControlID");
+            throw new HL7Exception(REQ_FLD_MISSING + "/MSH/MessageControlID");
         }
 
         String processingId = segment.getField(11, 0).encode();
         if (processingId == null || processingId.isEmpty()) {
-            throw new HL7Exception(REQ_FLD_MISSING+"/MSH/ProcessingID");
+            throw new HL7Exception(REQ_FLD_MISSING + "/MSH/ProcessingID");
         }
     }
 
@@ -45,44 +49,44 @@ public class OruR01Validator {
         if (!segment.isEmpty()) {
             String vendorOrg = segment.getField(1, 0).encode();
             if (vendorOrg == null || vendorOrg.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+locationName + "/SoftwareVendorOrganization");
+                throw new HL7Exception(REQ_FLD_MISSING + locationName + "/SoftwareVendorOrganization");
             }
             String versionNo = segment.getField(2, 0).encode();
             if (versionNo == null || versionNo.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+locationName + "/SoftwareCertifiedVersionOrReleaseNumber");
+                throw new HL7Exception(REQ_FLD_MISSING + locationName + "/SoftwareCertifiedVersionOrReleaseNumber");
             }
             String sftPrdName = segment.getField(3, 0).encode();
             if (sftPrdName == null || sftPrdName.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+locationName + "/SoftwareProductName");
+                throw new HL7Exception(REQ_FLD_MISSING + locationName + "/SoftwareProductName");
             }
             String sftBinaryId = segment.getField(4, 0).encode();
             if (sftBinaryId == null || sftBinaryId.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+locationName + "/SoftwareBinaryID");
+                throw new HL7Exception(REQ_FLD_MISSING + locationName + "/SoftwareBinaryID");
             }
         }
     }
 
     private static void patientValidator(Group group, String groupNameWithLocation) throws HL7Exception {
-        var patientCounter = group.getAll("PATIENT").length;
+        var patientCounter = group.getAll(PATIENT).length;
         if (patientCounter == 0) {
-            throw new HL7Exception(groupNameWithLocation+"PATIENT Group is Empty");
+            throw new HL7Exception(groupNameWithLocation + "PATIENT Group is Empty");
         }
         for (int i = 0; i < patientCounter; i++) {
-            var orderObsGroup = group.get("PATIENT", i);
+            var orderObsGroup = group.get(PATIENT, i);
 
             Group patientGroup = (Group) orderObsGroup;
             //PID
             var pid = (Segment) patientGroup.get("PID");
             String patientName = pid.getField(5, 0).encode();
             if (patientName == null || patientName.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+groupNameWithLocation + "PATIENT(" + i + ")/PID/PatientName");
+                throw new HL7Exception(REQ_FLD_MISSING + groupNameWithLocation + PATIENT + "(" + i + ")/PID/PatientName");
             }
             //NK1 - optional
             var nk1 = (Segment) patientGroup.get("NK1");
             if (!nk1.isEmpty()) {
                 String nk1Value = nk1.getField(1, 0).encode();
                 if (nk1Value == null || nk1Value.isEmpty()) {
-                    throw new HL7Exception(REQ_FLD_MISSING+groupNameWithLocation + "PATIENT(" + i + ")/NK1/SetID");
+                    throw new HL7Exception(REQ_FLD_MISSING + groupNameWithLocation + PATIENT + "(" + i + ")/NK1/SetID");
                 }
             }
         }
@@ -91,7 +95,7 @@ public class OruR01Validator {
     private static void orderObservationValidator(Group group, String parentGroup) throws HL7Exception {
         var orderCounter = group.getAll(ORDER_OBSERVATION).length;
         if (orderCounter == 0) {
-            throw new HL7Exception(parentGroup+ ORDER_OBSERVATION+" Group is Empty");
+            throw new HL7Exception(parentGroup + ORDER_OBSERVATION + " Group is Empty");
         }
         for (int i = 0; i < orderCounter; i++) {
             var orderObsGroup = (Group) group.get(ORDER_OBSERVATION, i);
@@ -116,12 +120,7 @@ public class OruR01Validator {
         var obr = (Segment) orderOBSGroup.get("OBR");
         String obr4Value = obr.getField(4, 0).encode();
         if (obr4Value == null || obr4Value.isEmpty()) {
-            throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/OBR/UniversalServiceID");
-        }
-        String[] obr4Values = getValuesFromField(obr4Value);
-        String universalServiceID = obr4Values[0];
-        if (universalServiceID.isEmpty()) {
-            throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/OBR/UniversalServiceID");
+            throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/OBR/UniversalServiceID");
         }
     }
 
@@ -133,7 +132,7 @@ public class OruR01Validator {
         if (!orc.isEmpty()) {
             String orc1Value = orc.getField(1, 0).encode();
             if (orc1Value == null || orc1Value.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/ORC/OrderControl");
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/ORC/OrderControl");
             }
         }
     }
@@ -146,7 +145,7 @@ public class OruR01Validator {
         if (!ctd.isEmpty()) {
             String ctd1Value = ctd.getField(1, 0).encode();
             if (ctd1Value == null || ctd1Value.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/CTD/ContactRole");
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/CTD/ContactRole");
             }
         }
     }
@@ -159,7 +158,7 @@ public class OruR01Validator {
         if (!cti.isEmpty()) {
             String cti1Value = cti.getField(1, 0).encode();
             if (cti1Value == null || cti1Value.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/CTI/SponsorStudyID");
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/CTI/SponsorStudyID");
             }
         }
     }
@@ -172,64 +171,47 @@ public class OruR01Validator {
         if (!ft1.isEmpty()) {
             String ft1Value4 = ft1.getField(4, 0).encode();
             if (ft1Value4 == null || ft1Value4.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/FT1/TransactionDate");
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/FT1/TransactionDate");
             }
             String ft1Value6 = ft1.getField(6, 0).encode();
             if (ft1Value6 == null || ft1Value6.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/FT1/TransactionType");
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/FT1/TransactionType");
             }
             String ft1Value7 = ft1.getField(7, 0).encode();
             if (ft1Value7 == null || ft1Value7.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/FT1/TransactionCode");
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/FT1/TransactionCode");
             }
         }
     }
 
     private static void obxValidator(Group orderObsGroup, int orderObsNo, String parentGroup) throws HL7Exception {
-        var obsCount = orderObsGroup.getAll("OBSERVATION").length;
+        var obsCount = orderObsGroup.getAll(OBSERVATION).length;
         for (int i = 0; i < obsCount; i++) {
-            var obsGrup = (Group) orderObsGroup.get("OBSERVATION", i);
+            var obsGrup = (Group) orderObsGroup.get(OBSERVATION, i);
             var obx = (Segment) obsGrup.get("OBX");
             //OBX - Observation Identifier (3-0)
             String obxIdentifier = obx.getField(3, 0).encode();
             if (obxIdentifier == null || obxIdentifier.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/OBSERVATION(" + i + ")/OBX/ObservationIdentifier");
-            } else {
-                String[] obxIdValues = getValuesFromField(obxIdentifier);
-                String obsID = obxIdValues[0];
-                if (obsID.isEmpty()) {
-                    throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/OBSERVATION(" + i + ")/OBX/ObservationIdentifier");
-                }
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/" + OBSERVATION + "(" + i + ")/OBX/ObservationIdentifier");
             }
             //OBX - ObservationResultStatus (11.0)
             String obsResultStatus = obx.getField(11, 0).encode();
             if (obsResultStatus == null || obsResultStatus.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/OBSERVATION(" + i + ")/OBX/ObservationResultStatus");
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/" + OBSERVATION + "(" + i + ")/OBX/ObservationResultStatus");
             }
         }
     }
 
     private static void spmValidator(Group orderObsGroup, int orderObsNo, String parentGroup) throws HL7Exception {
-        var spmCount = orderObsGroup.getAll("SPECIMEN").length;
+        var spmCount = orderObsGroup.getAll(SPECIMEN).length;
         for (int i = 0; i < spmCount; i++) {
-            var spmGrup = (Group) orderObsGroup.get("SPECIMEN", i);
+            var spmGrup = (Group) orderObsGroup.get(SPECIMEN, i);
             var spm = (Segment) spmGrup.get("SPM");
             //SPM - Specimen Type-4
             String specimenType = spm.getField(4, 0).encode();
             if (specimenType == null || specimenType.isEmpty()) {
-                throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/SPECIMEN(" + i + ")/SPM/SpecimenType");
-            } else {
-                String[] specimenTypeValues = getValuesFromField(specimenType);
-                String specimenTypeID = specimenTypeValues[0];
-                if (specimenTypeID.isEmpty()) {
-                    throw new HL7Exception(REQ_FLD_MISSING+parentGroup + ORDER_OBSERVATION+"(" + orderObsNo + ")/SPECIMEN(" + i + ")/SPM/SpecimenType");
-                }
+                throw new HL7Exception(REQ_FLD_MISSING + parentGroup + ORDER_OBSERVATION + "(" + orderObsNo + ")/" + SPECIMEN + "(" + i + ")/SPM/SpecimenType");
             }
         }
-    }
-
-    private static String[] getValuesFromField(String fieldValues) {
-        //String str="^Whether this is the patient's first test for the condition of interest^LN^^^^2.69";//NOSONAR
-        return fieldValues.split("\\^", -1);
     }
 }
