7782d4f6530025c935e78d662b42044a990118f8
Snehaa Shri Hari <snehaashrihari@lt-aplwcfhxvp67k.attlocal.net>
2025-01-23 19:46:56 -0500

    fixing errors and test cases'

--- deduplication/src/main/java/gov/cdc/nbs/deduplication/seed/mapper/DeduplicationEntryMapper.java
+++ deduplication/src/main/java/gov/cdc/nbs/deduplication/seed/mapper/DeduplicationEntryMapper.java
@@ -2,6 +2,8 @@ package gov.cdc.nbs.deduplication.seed.mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.lang.NonNull;
@@ -13,12 +15,16 @@ public class DeduplicationEntryMapper implements RowMapper<DeduplicationEntry> {
   @Override
   @Nullable
   public DeduplicationEntry mapRow(@NonNull ResultSet rs, int rowNum) throws SQLException {
+    Timestamp timestamp = rs.getTimestamp("processed_at");
+    LocalDateTime processedAt = (timestamp != null) ? timestamp.toLocalDateTime() : null;
+
     return new DeduplicationEntry(
         rs.getLong("person_uid"),
         rs.getLong("person_parent_uid"),
         rs.getString("mpi_patient_uuid"),
         rs.getString("mpi_person_uuid"),
-        rs.getString("status")
+        rs.getString("status"),
+        processedAt
     );
 
   }
--- deduplication/src/main/java/gov/cdc/nbs/deduplication/seed/model/DeduplicationEntry.java
+++ deduplication/src/main/java/gov/cdc/nbs/deduplication/seed/model/DeduplicationEntry.java
@@ -1,10 +1,12 @@
 package gov.cdc.nbs.deduplication.seed.model;
+import java.time.LocalDateTime;
 
 public record DeduplicationEntry(
     Long nbsPersonId,
     Long nbsPersonParentId,
     String mpiPatientId,
     String mpiPersonId,
-    String status){
+    String status,
+    LocalDateTime processed_at){
 
 }
