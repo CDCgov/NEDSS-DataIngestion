f646f56d42519cf60e7a1c01ba0d590b3eb76bb2
Michael Peels <109251240+mpeels@users.noreply.github.com>
2025-01-27 10:00:22 -0500

    Update model for RecordLinker v25.1.0 (#289)
    * Update seed and match mappers to align with new Record Linkage version
    
    * Initialize batch tables
    
    * Additional test
    
    ---------
    
    Co-authored-by: Michael Peels <michaelpeels@Michael-Peels.local>

--- deduplication/src/main/java/gov/cdc/nbs/deduplication/seed/model/SeedRequest.java
+++ deduplication/src/main/java/gov/cdc/nbs/deduplication/seed/model/SeedRequest.java
@@ -10,41 +10,4 @@ public record SeedRequest(List<Cluster> clusters) {
   ) {
   }
 
-  public record MpiPerson(
-      String external_id, // person_uid
-      String parent_id,
-      String birth_date,
-      String sex,
-      String mrn,
-      List<Address> address,
-      List<Name> name,
-      List<Telecom> telecom,
-      String ssn,
-      String race,
-      String gender,
-      DriversLicense drivers_license) {
-  }
-
-  public record Address(
-      List<String> line,
-      String city,
-      String state,
-      String postal_code,
-      String county) {
-  }
-
-  public record Name(
-      List<String> given,
-      String family,
-      List<String> suffix) {
-  }
-
-  public record Telecom(String value) {
-  }
-
-  public record DriversLicense(
-      String value,
-      String authority) {
-  }
-
 }
