ecea563f8edf9c7faf47c599f00ef37f54eed511
Michael Peels <michaelpeels@Michael-Peels.local>
2025-02-05 10:11:48 -0500

    EcrContainerMapper testing

--- data-ingestion-service/src/test/java/gov/cdc/dataingestion/nbs/ecr/resolver/mapper/EcrContainerMapperTest.java
+++ data-ingestion-service/src/test/java/gov/cdc/dataingestion/nbs/ecr/resolver/mapper/EcrContainerMapperTest.java
@@ -13,27 +13,27 @@ import gov.cdc.dataingestion.nbs.repository.model.dto.EcrMsgContainerDto;
 
 class EcrContainerMapperTest {
 
-  private final EcrContainerMapper mapper = new EcrContainerMapper();
+    private final EcrContainerMapper mapper = new EcrContainerMapper();
 
-  @Test
-  void shouldMapFields() throws SQLException {
-    ResultSet rs = Mockito.mock(ResultSet.class);
-    when(rs.getInt("msgContainerUid")).thenReturn(1);
-    when(rs.getString("invLocalId")).thenReturn("invLocalId");
-    when(rs.getInt("nbsInterfaceUid")).thenReturn(2);
-    when(rs.getString("receivingSystem")).thenReturn("receivingSystem");
-    when(rs.getString("ongoingCase")).thenReturn("ongoingCase");
-    when(rs.getInt("versionCtrlNbr")).thenReturn(3);
-    when(rs.getInt("dataMigrationStatus")).thenReturn(4);
-    EcrMsgContainerDto actual = mapper.mapRow(rs, 0);
+    @Test
+    void shouldMapFields() throws SQLException {
+        ResultSet rs = Mockito.mock(ResultSet.class);
+        when(rs.getInt("msgContainerUid")).thenReturn(1);
+        when(rs.getString("invLocalId")).thenReturn("invLocalId");
+        when(rs.getInt("nbsInterfaceUid")).thenReturn(2);
+        when(rs.getString("receivingSystem")).thenReturn("receivingSystem");
+        when(rs.getString("ongoingCase")).thenReturn("ongoingCase");
+        when(rs.getInt("versionCtrlNbr")).thenReturn(3);
+        when(rs.getInt("dataMigrationStatus")).thenReturn(4);
+        EcrMsgContainerDto actual = mapper.mapRow(rs, 0);
 
-    assertThat(actual.msgContainerUid()).isEqualTo(1);
-    assertThat(actual.invLocalId()).isEqualTo("invLocalId");
-    assertThat(actual.nbsInterfaceUid()).isEqualTo(2);
-    assertThat(actual.receivingSystem()).isEqualTo("receivingSystem");
-    assertThat(actual.ongoingCase()).isEqualTo("ongoingCase");
-    assertThat(actual.versionCtrlNbr()).isEqualTo(3);
-    assertThat(actual.dataMigrationStatus()).isEqualTo(4);
+        assertThat(actual.msgContainerUid()).isEqualTo(1);
+        assertThat(actual.invLocalId()).isEqualTo("invLocalId");
+        assertThat(actual.nbsInterfaceUid()).isEqualTo(2);
+        assertThat(actual.receivingSystem()).isEqualTo("receivingSystem");
+        assertThat(actual.ongoingCase()).isEqualTo("ongoingCase");
+        assertThat(actual.versionCtrlNbr()).isEqualTo(3);
+        assertThat(actual.dataMigrationStatus()).isEqualTo(4);
 
-  }
+    }
 }
