0ce93c1d6c98396a6744ba3f1ac474e96c934003
Sheham Izat <sizat@enquizit.com>
2025-02-27 18:04:42 -0500

    Configure Debezium during container setup by updating Dockerfile and initialization scripts

--- containers/db/initialize/initialize.sh
+++ containers/db/initialize/initialize.sh
@@ -10,9 +10,15 @@ echo "*************************************************************************"
 echo "  Initializing NBS databases"
 echo "*************************************************************************"
 
-for sql in $(find "$BASE/restore.d" -iname "*.sql" | sort) ; 
+# Enable CLR
+echo "*************************************************************************"
+echo "  Enabling CLR"
+echo "*************************************************************************"
+/opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -Q "EXEC sp_configure 'clr enabled', 1; RECONFIGURE;"
+
+for sql in $(find "$BASE/restore.d" -iname "*.sql" | sort) ;
 do
-    echo "Executing: $sql"  
+    echo "Executing: $sql"
     /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -i "$sql"
 
     echo "Completed: $sql"
@@ -20,4 +26,25 @@ done
 
 echo "*************************************************************************"
 echo "  NBS databases ready"
+echo "*************************************************************************"
+
+
+# Enable CDC at the database level
+echo "*************************************************************************"
+echo "  Enabling CDC at the database level"
+echo "*************************************************************************"
+if ! /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -Q "USE NBS_ODSE; EXEC sp_changedbowner 'sa'; EXEC sys.sp_cdc_enable_db;"; then
+    echo "Error enabling CDC at the database level"
+    exit 1
+fi
+# Enable CDC for each table
+echo "*************************************************************************"
+echo "  Enabling CDC for tables"
+echo "*************************************************************************"
+for table in "Person" "Person_Name" "Person_Race" "Entity_Id" "Tele_Locator" "Postal_Locator"; do
+    echo "Enabling CDC for table: $table"
+    /opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -Q "USE NBS_ODSE; EXEC sys.sp_cdc_enable_table @source_schema = 'dbo', @source_name = '$table', @role_name = NULL;"
+done
+echo "*************************************************************************"
+echo " CDC has been enabled."
 echo "*************************************************************************"
