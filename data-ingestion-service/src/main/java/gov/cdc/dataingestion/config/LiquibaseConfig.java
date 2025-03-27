456e276ea027812634dc2d1a03fd2f389eb8b910
Ragul Shanmugam <rshanmugam@enquizit.com>
2025-03-27 08:31:33 -0400

    Added Liquibase config class

---
+++ data-ingestion-service/src/main/java/gov/cdc/dataingestion/config/LiquibaseConfig.java
@@ -0,0 +1,55 @@
+package gov.cdc.dataingestion.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Value("${spring.liquibase.user}")
+    private String dbUserName;
+
+    @Value("${spring.liquibase.password}")
+    private String dbUserPassword;
+
+    @Value("${spring.liquibase.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.liquibase.url}")
+    private String dbUrl;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.liquibase")
+    public LiquibaseProperties liquibaseProperties() {
+        return new LiquibaseProperties();
+    }
+
+    @Bean(name = "liquibaseDataSource")
+    public DataSource liquibaseDataSource() {
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(dbUserName)
+                .password(dbUserPassword)
+                .driverClassName(driverClassName)
+                .build();
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(@Qualifier("liquibaseDataSource") DataSource liquibaseDataSource, LiquibaseProperties liquibaseProperties) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(liquibaseDataSource);
+        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
+        if (liquibaseProperties.getContexts() != null) {
+            liquibase.setContexts(String.join(",", liquibaseProperties.getContexts()));
+        }
+        return liquibase;
+    }
+}
--- data-ingestion-service/src/main/resources/db/di-service-002.sql
+++ data-ingestion-service/src/main/resources/db/di-service-002.sql
@@ -1,5 +1,6 @@
-ALTER TABLE elr_raw
+USE NBS_DataIngest;
+ALTER TABLE NBS_DataIngest.dbo.elr_raw
     ADD version VARCHAR(1);
 
-ALTER TABLE elr_dlt
+ALTER TABLE NBS_DataIngest.dbo.elr_dlt
     ALTER COLUMN dlt_status NVARCHAR(30);
