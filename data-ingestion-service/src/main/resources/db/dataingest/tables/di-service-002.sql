3088a44305ffef92e086e8dc9b5d11811d3c2137
Duc Nguyen <dnguyen@enquizit.com>
2025-04-07 11:44:29 -0700

    update

--- data-ingestion-service/src/main/resources/db/dataingest/tables/di-service-002.sql
+++ data-ingestion-service/src/main/resources/db/dataingest/tables/di-service-002.sql
@@ -1,6 +1,24 @@
 USE NBS_DataIngest;
-ALTER TABLE NBS_DataIngest.dbo.elr_raw
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'dbo'
+      AND TABLE_NAME = 'elr_raw'
+      AND COLUMN_NAME = 'version'
+)
+BEGIN
+ALTER TABLE dbo.elr_raw
     ADD version VARCHAR(1);
+END
 
-ALTER TABLE NBS_DataIngest.dbo.elr_dlt
-    ALTER COLUMN dlt_status NVARCHAR(30);
+IF EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'dbo'
+      AND TABLE_NAME = 'elr_dlt'
+      AND COLUMN_NAME = 'dlt_status'
+)
+BEGIN
+ALTER TABLE dbo.elr_dlt
+ALTER COLUMN dlt_status NVARCHAR(30);
+END
