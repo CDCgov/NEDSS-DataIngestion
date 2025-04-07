186a0ce5c515117d237dcc66955a920e5767f5ec
Duc Nguyen <117302958+ndduc01@users.noreply.github.com>
2025-04-07 10:38:15 -0700

    Cnde 2458 - ECR Liquibase (#330)
    * update to support multi DB liquibase
    
    * Revert "Revert "Cnde 2375 liquibase di (#319)" (#327)"
    
    This reverts commit 67fe4fd9bbe904471f3bc7c928a0fd6b8794aa0b.
    
    * Added support for Msgoute ECR liquibase
    
    * update to run on change

--- data-ingestion-service/src/main/resources/db/dataingest/tables/di-service-002.sql
+++ data-ingestion-service/src/main/resources/db/dataingest/tables/di-service-002.sql
@@ -1,5 +1,6 @@
+USE NBS_DataIngest;
 ALTER TABLE NBS_DataIngest.dbo.elr_raw
     ADD version VARCHAR(1);
 
 ALTER TABLE NBS_DataIngest.dbo.elr_dlt
-ALTER COLUMN dlt_status NVARCHAR(30);
+    ALTER COLUMN dlt_status NVARCHAR(30);
