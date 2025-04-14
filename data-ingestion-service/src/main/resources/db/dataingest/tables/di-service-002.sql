4b3ff41aeffa65d36c0c4420db160174706fb74f
Duc Nguyen <dnguyen@enquizit.com>
2025-04-14 08:30:15 -0700

    vulnerability

--- data-ingestion-service/src/main/resources/db/dataingest/tables/di-service-002.sql
+++ data-ingestion-service/src/main/resources/db/dataingest/tables/di-service-002.sql
@@ -1,6 +1,26 @@
 USE NBS_DataIngest;
-ALTER TABLE NBS_DataIngest.dbo.elr_raw
-    ADD version VARCHAR(1);
 
-ALTER TABLE NBS_DataIngest.dbo.elr_dlt
-    ALTER COLUMN dlt_status NVARCHAR(30);
+IF NOT EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'dbo'
+      AND TABLE_NAME = 'elr_raw'
+      AND COLUMN_NAME = 'version'
+)
+BEGIN
+ALTER TABLE dbo.elr_raw
+    ADD version VARCHAR(1);
+END
+
+IF EXISTS (
+    SELECT 1
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_SCHEMA = 'dbo'
+      AND TABLE_NAME = 'elr_dlt'
+      AND COLUMN_NAME = 'dlt_status'
+      AND (DATA_TYPE != 'nvarchar' OR CHARACTER_MAXIMUM_LENGTH != 30)
+)
+BEGIN
+ALTER TABLE dbo.elr_dlt
+ALTER COLUMN dlt_status NVARCHAR(30);
+END
