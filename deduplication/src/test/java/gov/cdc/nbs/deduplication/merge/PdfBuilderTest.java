b00dd7eecaecd7046d533092d269a78ca5845096
Michael Peels <michaelpeels@Michael-Peels.local>
2025-05-09 15:13:44 -0400

    Fix pdf builder test import

--- deduplication/src/test/java/gov/cdc/nbs/deduplication/merge/PdfBuilderTest.java
+++ deduplication/src/test/java/gov/cdc/nbs/deduplication/merge/PdfBuilderTest.java
@@ -11,7 +11,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.util.List;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.lowagie.text.pdf.PdfReader;
 
