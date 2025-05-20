3b0583c7d996bf89cbf9e4710c8d478ca315f45f
snehaasjournal <uob6@cdc.gov>
2025-05-20 07:20:07 -0700

    Add unit test

--- deduplication/src/test/java/gov/cdc/nbs/deduplication/merge/PatientMergeControllerTest.java
+++ deduplication/src/test/java/gov/cdc/nbs/deduplication/merge/PatientMergeControllerTest.java
@@ -164,6 +164,35 @@ class PatientMergeControllerTest {
     );
   }
 
+  @Test
+  void testExportMatchesAsCSV() throws Exception {
+    List<MatchRequiringReview> mockMatches = List.of(
+            new MatchRequiringReview("111122", "John Smith", "2023-01-01T10:00:00Z", "2023-01-05T15:00:00Z", 2),
+            new MatchRequiringReview("111133", "Andrew James", "2023-02-02T11:00:00Z", "2023-02-06T16:30:00Z", 4)
+    );
+
+    when(matchesRequiringReviewResolver.resolveAll(PatientMergeController.DEFAULT_SORT)).thenReturn(mockMatches);
+    when(pdfBuilder.formatDateTime("2023-01-01T10:00:00Z")).thenReturn("01/01/2023 10:00 AM");
+    when(pdfBuilder.formatDateTime("2023-01-05T15:00:00Z")).thenReturn("01/05/2023 03:00 PM");
+    when(pdfBuilder.formatDateTime("2023-02-02T11:00:00Z")).thenReturn("02/02/2023 11:00 AM");
+    when(pdfBuilder.formatDateTime("2023-02-06T16:30:00Z")).thenReturn("02/06/2023 04:30 PM");
+
+    mockMvc.perform(get("/merge/export/csv"))
+            .andExpect(status().isOk())
+            .andExpect(header().string("Content-Type", "text/csv"))
+            .andExpect(header().string("Content-Disposition", "attachment; filename=matches_requiring_review.csv"))
+            .andExpect(content().string(
+                    """
+                    Patient ID,Person Name,Date Created,Date Identified,Number of Matching Records
+                    "111122","John Smith","01/01/2023 10:00 AM","01/05/2023 03:00 PM",2
+                    "111133","Andrew James","02/02/2023 11:00 AM","02/06/2023 04:30 PM",4
+                    """));
+
+    verify(matchesRequiringReviewResolver).resolveAll(PatientMergeController.DEFAULT_SORT);
+    verify(pdfBuilder, times(4)).formatDateTime(anyString());
+  }
+
+
   private List<PersonMergeData> expectedPersonMergeData() {
     return List.of(
         new PersonMergeData(
