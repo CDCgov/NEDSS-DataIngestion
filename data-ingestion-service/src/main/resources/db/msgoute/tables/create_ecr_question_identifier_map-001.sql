27ee1621e83b9dc3a4d60dac37a539768a33efba
Ragul Shanmugam <rshanmugam@enquizit.com>
2025-06-02 08:51:12 -0700

    Syntax fix for ecr lookup liquibase scripts

--- data-ingestion-service/src/main/resources/db/msgoute/tables/create_ecr_question_identifier_map-001.sql
+++ data-ingestion-service/src/main/resources/db/msgoute/tables/create_ecr_question_identifier_map-001.sql
@@ -1,9 +1,13 @@
-USE [NBS_MSGOUTE]
-GO
-
+USE NBS_MSGOUTE;
+IF
+NOT EXISTS(
+        SELECT 'X'
+        FROM INFORMATION_SCHEMA.TABLES
+        WHERE TABLE_NAME = 'ecr_question_identifier_map')
+BEGIN
 CREATE TABLE [dbo].[ecr_question_identifier_map](
     [COLUMN_NM] [varchar](255) NULL,
     [QUESTION_IDENTIFIER] [varchar](255) NULL,
     [DYNAMIC_QUESTION_IDENTIFIER] [varchar](255) NULL
     ) ON [PRIMARY]
-    GO
+END
