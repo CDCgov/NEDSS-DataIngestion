d6fee8e55386903f43348abee3c736ff763eb05c
Michael Peels <m.peels.dev@gmail.com>
2025-06-05 13:46:30 -0400

    Update test with new field name

--- deduplication/src/test/java/gov/cdc/nbs/deduplication/merge/PatientMergeControllerTest.java
+++ deduplication/src/test/java/gov/cdc/nbs/deduplication/merge/PatientMergeControllerTest.java
@@ -306,7 +306,7 @@ class PatientMergeControllerTest {
               "deathCounty": "Anderson County",
               "deathCountry": "Afghanistan"
             },
-            "generalPatientInformation": {
+            "general": {
               "asOf": "2025-05-27T00:00:00",
               "maritalStatus": "Annulled",
               "mothersMaidenName": "MotherMaiden",
