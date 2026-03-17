81f2dd41c61a7720a5ff64ceba686f7b1b7311f2
Ada Ball <ada@adamball.org>
2026-03-17 11:47:45 -0500

    remove srte-data-service directory
    I did this in a previous commit but after recreating
    the branch locally due to some GitHub weirdness
    it appears to still exist

--- srte-data-service/src/main/java/gov/cdc/srtedataservice/exception/RtiSecurityException.java
+++
@@ -1,9 +0,0 @@
-package gov.cdc.srtedataservice.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class RtiSecurityException extends AuthenticationException {
-  public RtiSecurityException(String message) {
-    super(message);
-  }
-}
