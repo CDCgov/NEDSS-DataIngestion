0bfa71e634f6ecb4d6230ea7831c129e058f758f
Michael Peels <m.peels.dev@gmail.com>
2026-06-24 16:38:44 -0400

    Add logging to containers. Remove var from workflow

--- deduplication/src/test/java/gov/cdc/nbs/deduplication/config/container/ComposeContainerInitializer.java
+++ deduplication/src/test/java/gov/cdc/nbs/deduplication/config/container/ComposeContainerInitializer.java
@@ -2,15 +2,19 @@ package gov.cdc.nbs.deduplication.config.container;
 
 import java.io.File;
 import java.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.lang.NonNull;
 import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 
 class ComposeContainerInitializer
     implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
+  private static final Logger log = LoggerFactory.getLogger(ComposeContainerInitializer.class);
+  private static final Slf4jLogConsumer consumer = new Slf4jLogConsumer(log);
   private static final File composeFile = new File("../docker-compose.yml");
   private static boolean started = false;
 
@@ -22,6 +26,8 @@ class ComposeContainerInitializer
           // List specific services
           .withServices("nbs-mssql", "di-record-linker")
           .waitingFor("nbs-mssql", Wait.forHealthcheck())
+          .withLogConsumer("nbs-mssql", consumer)
+          .withLogConsumer("di-record-linker", consumer)
           .withStartupTimeout(Duration.ofMinutes(10));
 
   @Override
